434eae08957f871ddae1f226c7a6469bd1e2452d
Daniel Jones <daniel@jones.org.uk>
2015-11-06 17:11:55 +0000

    Add Noise generator, with linear interpolation

---
+++ signum/gen/noise.cpp
@@ -0,0 +1,52 @@
+#include "noise.h"
+
+#include "stdlib.h"
+
+
+namespace signum::gen
+{
+
+Noise::Noise(float frequency, bool interpolate)
+{
+	this->frequency = frequency;
+	this->interpolate = interpolate;
+	this->value = (float) rand() / RAND_MAX;
+
+	this->steps_remaining = 0;
+}
+
+void Noise::next(int count)
+{
+	for (int i = 0; i < count; i++)
+	{
+		if (this->steps_remaining <= 0)
+		{
+			// pick a new target value
+			float target = (float) rand() / RAND_MAX;
+
+			if (this->frequency > 0)
+			{
+				this->steps_remaining = rand() % (int) (44100.0 / this->frequency);
+				this->step_change = (target - value) / this->steps_remaining;
+			}
+			else
+			{
+				this->steps_remaining = 0;
+				this->step_change = target - value;
+			}
+
+			if (!this->interpolate)
+			{
+				this->value = target;
+				this->step_change = 0;
+			}
+		}
+
+		this->value += this->step_change;
+
+		this->output->data[0][i] = this->value;
+		this->steps_remaining--;
+	}
+}
+
+}
---
+++ signum/gen/noise.h
@@ -0,0 +1,24 @@
+#pragma once 
+
+#include "../unit.h"
+
+namespace signum::gen
+{
+	class Noise : public Unit
+	{
+	public:
+		Noise(float frequency = 0, bool interpolate = false);
+
+		float frequency;
+		bool interpolate;
+		int phase;
+		sample value;
+
+		virtual void next(int count);
+	
+	private:
+		int steps_remaining;
+		float step_change;
+
+	};
+}
