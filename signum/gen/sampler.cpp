357e23753f8a4b95b5e5edbe9770eadce75880b7
Daniel Jones <daniel@jones.org.uk>
2015-11-07 14:45:03 +0000

    Remove Sampler cruft

--- signum/gen/sampler.cpp
+++ signum/gen/sampler.cpp
@@ -1,45 +1,10 @@
-#include "sampler.h"
-
+#include "sampler.h" 
 #include "constant.h"
 
-#include <sndfile.h>
 #include <stdlib.h>
 
 namespace signum::gen
 {
-
-/*
-Sampler::Sampler(const char *filename, float rate, bool loop)
-{
-	this->rate = new gen::Constant(rate);
-	this->loop = loop;
-
-	SF_INFO info;
-	SNDFILE *sndfile = sf_open(filename, SFM_READ, &info);
-
-	if (!sndfile)
-	{
-		printf("Failed to read soundfile\n");
-		exit(1);
-	}
-
-	this->buffer = (float *) malloc(sizeof(double) * info.frames);
-	// printf("Sample read: %p, frames = %lld\n", sndfile, info.frames);
-	int ptr = 0;
-
-	while (true)
-	{
-		int count = sf_readf_float(sndfile, this->buffer + ptr, 2048);
-		if (count == 0)
-			break;
-
-		ptr += count;
-	}
-
-	this->phase = 0;
-	this->num_frames = info.frames;
-}
-*/
 
 Sampler::Sampler(const char *filename, float rate, bool loop)
 {
