f0f2beccd381e16e5c2a3196c705ae0fe9339e92
Daniel Jones <daniel@jones.org.uk>
2015-11-07 18:11:30 +0000

    Add ringbuffer implementation

---
+++ signum/ringbuffer.h
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+
+template <class T>
+class RingBuffer
+{
+	public:
+		RingBuffer(int size);
+		~RingBuffer();
+
+		void add(T value);
+		T get(int index);
+		T operator [](int index){ return this->get(index); }
+
+	private:
+		T *data;
+		int size;
+		int position;
+};
+
+
+template <class T>
+RingBuffer<T>::RingBuffer(int size)
+{
+	this->data = (T *) malloc(sizeof(T) * size);
+	this->position = 0;
+	this->size = size;
+}
+
+template <class T>
+RingBuffer<T>::~RingBuffer()
+{
+	free(this->data);
+}
+
+template <class T>
+void RingBuffer<T>::add(T value)
+{
+	this->data[this->position] = value;
+	this->position = (this->position + 1) % this->size;
+}
+
+template <class T>
+T RingBuffer<T>::get(int index)
+{
+	int new_index = index + this->position - 1;
+	new_index = (new_index % this->size);
+	return data[new_index];
+}
+
