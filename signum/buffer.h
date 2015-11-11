b4e93cf2de6d4892b84b492366bbc758886868d6
Daniel Jones <daniel@jones.org.uk>
2015-11-11 21:29:50 +0000

    Buffer: Add get and operator[] convenience methods

--- signum/buffer.h
+++ signum/buffer.h
@@ -8,10 +8,13 @@ namespace signum
 	{
 	public:
 		Buffer(int num_channels, int num_frames);
-
 		Buffer(const char *filename);
 
 		void open(const char *filename);
+
+		// TODO: Should we assert a buffer is mono before indexing?
+		sample get(int index) { return this->data[0][index]; }
+		sample operator [](int index){ return this->get(index); }
 
 		int num_channels;
 		int num_frames;
