7f9dbe367003016f477f40ba975b03e214996c36
Daniel Jones <daniel@jones.org.uk>
2015-11-13 21:18:22 +0000

    Update signum-test for new syntax

--- signum-test.cpp
+++ signum-test.cpp
@@ -5,40 +5,40 @@
 
 using namespace signum;
 
-io::AudioOut output;
+Graph *graph = new Graph();
 
 void sine_test()
 {
-	gen::Sine *sine = new gen::Sine(440.0);
-	sine->route(output);
+	UnitRef sine = new gen::Sine(440.0);
+	sine->route(graph->output);
 }
 
 void buffer_test()
 {
 	Buffer *buffer = new Buffer("gliss.aif");
-	Unit *dust = new rnd::Dust(1);
-	Unit *pos = new rnd::Noise(0.05, true, 0, buffer->duration);
-	Unit *granulator = new gen::Granulator(buffer, dust, pos, 0.3);
-	fx::Delay *delay = new fx::Delay(granulator, 0.2, 0.5);
+	UnitRef dust = new rnd::Dust(1);
+	UnitRef pos = new rnd::Noise(0.05, true, 0, buffer->duration);
+	UnitRef granulator = new gen::Granulator(buffer, dust, pos, 0.3);
+	UnitRef delay = new fx::Delay(granulator, 0.2, 0.5);
 
-	delay->route(output);
+	delay->route(graph->output);
 }
 
 void spooky_wobble_test()
 {
-    gen::Sampler *sampler = new gen::Sampler("gliss.aif");
-    sampler->loop = true;
-    sampler->rate = new rnd::Noise(0.3, true);
+	Buffer *buffer = new Buffer("gliss.aif");
+    UnitRef sampler = new gen::Sampler(buffer, 1, true);
+    sampler->set_param("rate", new rnd::Noise(0.3, true));
 
-    sampler->route(output);
+    sampler->route(graph->output);
 
-    rnd::Noise *noise = new rnd::Noise(50, true);
-    noise->route(output);
+    UnitRef noise = new rnd::Noise(50, true);
+    noise->route(graph->output);
 
-    rnd::Dust *dust = new rnd::Dust(1);
-    dust->route(output);
+    UnitRef dust = new rnd::Dust(1);
+    dust->route(graph->output);
 
-	output.start();
+	graph->run();
 }
 
 int main()
@@ -48,6 +48,6 @@ int main()
 	// spooky_wobble_test();
 	// sine_test();
 	buffer_test();
-	output.start();
+	graph->run();
 }
 
