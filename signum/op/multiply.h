3b83d81fdee2dec013d0171c65514fba257dad6c
Daniel Jones <daniel@jones.org.uk>
2015-11-13 23:39:38 +0000

    Multiply: Move logic to .h file

--- signum/op/multiply.h
+++ signum/op/multiply.h
@@ -1,6 +1,7 @@
 #include "../constants.h"
 
 #include "../unit.h"
+#include "../gen/constant.h"
 
 namespace signum::op
 {
@@ -10,14 +11,17 @@ class Multiply : public Unit
 
 public:
 
-	Multiply(UnitRef a, UnitRef b);
-	Multiply(UnitRef a, sample b);
+	Multiply(UnitRef a, UnitRef b) : a(a), b(b) {};
+	Multiply(UnitRef a, sample b) : a(a), b(new gen::Constant(b)) {};
 	Multiply(sample a, UnitRef b);
 
 	UnitRef a;
 	UnitRef b;
 
-	virtual sample next();
+	virtual sample next()
+	{
+		return this->a->next() * this->b->next();
+	}
 };
 
 }
