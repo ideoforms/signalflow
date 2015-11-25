5c0318ad8aa4819b6a5bfd23ead2a6cc3f98e180
Daniel Jones <daniel@jones.org.uk>
2015-11-25 22:33:12 +0000

    Move all Graph logic to .cpp

--- signum/graph.cpp
+++ signum/graph.cpp
@@ -1,17 +1,48 @@
 #include "graph.h"
 #include "signum.h"
-
 #include "io/output.h"
 
 namespace signum
 {
+	Graph::Graph()
+	{
+		signum_init();
+		this->output = new io::AudioOut(this);
+	}
 
-Graph::Graph()
-{
-	signum_init();
-	
-	// input = new io::AudioIn();
-	this->output = new io::AudioOut(this);
-} 
+	void Graph::run()
+	{
+		while (true) { usleep(100000); }
+	}
 
-};
+	void Graph::pull_input(UnitRef &unit, int num_frames)
+	{
+		// Must pull our input's outputs first,
+		// or pops will be caused in the output (something to do with phase?)
+		for (UnitRef input : unit->inputs)
+		{
+			if (!input)
+				fprintf(stderr, "Unit %s has a null input\n", unit->name.c_str());
+			this->pull_input(input, num_frames);
+		}
+		for (auto param : unit->params)
+		{
+			UnitRef param_unit = *(param.second);
+			if (param_unit)
+				this->pull_input(param_unit, num_frames);
+			else
+				fprintf(stderr, "Unit %s has a null param input (parameter: %s)\n", unit->name.c_str(), param.first.c_str());
+		}
+		unit->next(unit->out, num_frames);
+	}
+
+	void Graph::pull_input(int num_frames)
+	{
+		this->pull_input(this->output, num_frames);
+	}
+
+	UnitRef Graph::addUnit(Unit *unit)
+	{
+		return std::shared_ptr<Unit>(unit);
+	}
+}
--- signum/graph.h
+++ signum/graph.h
@@ -2,8 +2,6 @@
 
 #include "unit.h"
 
-#include <unistd.h>
-
 namespace signum
 {
 
@@ -13,36 +11,10 @@ namespace signum
 
 			Graph();
 
-			void run()
-			{
-				while (true) { usleep(100000); }
-			}
-
-			void pull_input(UnitRef &unit, int num_frames)
-			{
-				// Must pull our input's outputs first,
-				// or pops will be caused in the output (something to do with phase?)
-				for (UnitRef input : unit->inputs)
-				{
-					this->pull_input(input, num_frames);
-				}
-				for (auto param : unit->params)
-				{
-					UnitRef param_unit = *(param.second);
-					this->pull_input(param_unit, num_frames);
-				}
-				unit->next(unit->out, num_frames);
-			}
-
-			void pull_input(int num_frames)
-			{
-				this->pull_input(this->output, num_frames);
-			}
-
-			UnitRef addUnit(Unit *unit)
-			{
-				return std::shared_ptr<Unit>(unit);
-			}
+			void run();
+			void pull_input(UnitRef &unit, int num_frames);
+			void pull_input(int num_frames);
+			UnitRef addUnit(Unit *unit);
 
 			UnitRef input = nullptr;
 			UnitRef output = nullptr;
