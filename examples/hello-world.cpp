a105129cfacb1a03345b973b01191acacc559866
Daniel Jones <daniel@jones.org.uk>
2015-12-02 21:52:56 +0000

    Add Hello World example

---
+++ examples/hello-world.cpp
@@ -0,0 +1,32 @@
+#include <signum/signum.h>
+
+/*------------------------------------------------------------------------
+ * All objects are in the signum:: namespace.
+ * Import this namespace for code brevity.
+ *-----------------------------------------------------------------------*/
+using namespace signum;
+
+int main()
+{
+	/*------------------------------------------------------------------------
+	 * Instantiate a single Graph object for all global audio processing.
+	 *-----------------------------------------------------------------------*/
+	Graph *graph = new Graph();
+
+	/*------------------------------------------------------------------------
+	 * 440hz Hello World
+	 *-----------------------------------------------------------------------*/
+	UnitRef sine = new gen::Sine(440);
+
+	/*------------------------------------------------------------------------
+	 * The Graph can have multiple inputs, summed to output.
+	 *-----------------------------------------------------------------------*/
+	graph->output->add_input(sine);
+
+	/*------------------------------------------------------------------------
+	 * Graph actually begins processing as soon as inputs are added.
+	 * run() really just loops indefinitely.
+	 *-----------------------------------------------------------------------*/
+	graph->run();
+}
+
