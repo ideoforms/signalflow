b983d84c4bcf7938085a5a0219ed25f9d6cfda5a
Daniel Jones <daniel@jones.org.uk>
2015-12-07 18:58:42 +0000

    Add synthdef example to examples

---
+++ examples/synthdef-example.cpp
@@ -0,0 +1,43 @@
+#include "signum/signum.h"
+
+using namespace signum;
+
+int main()
+{
+	Graph *graph = new Graph();
+
+	/*------------------------------------------------------------------------
+	 * Create a new SynthDef (with optional name)
+	 *-----------------------------------------------------------------------*/
+	SynthDef *def = new SynthDef();
+
+	/*------------------------------------------------------------------------
+	 * Create a named input that can be used to modulate parameters of
+	 * the synth.
+	 *-----------------------------------------------------------------------*/
+	UnitRef base_freq = def->add_input("base_freq", 40.0);
+	UnitRef freq = def->add_node(new rnd::Noise(2.0, true, 40, 160));
+	UnitRef sine = def->add_node(new gen::Sine(freq + base_freq));
+
+	/*------------------------------------------------------------------------
+	 * Set the output of the synth.
+	 *-----------------------------------------------------------------------*/
+	def->set_output(sine);
+
+	/*------------------------------------------------------------------------
+	 * Instantiate a synth that uses this definition.
+	 *-----------------------------------------------------------------------*/
+	Synth *synth1 = new Synth(def);
+	graph->output->add_input(synth1->output);
+
+	while (true)
+	{
+		float freq = rng_uniform(40, 400);
+		synth1->set_param("base_freq", freq);
+		usleep(500000);
+	}
+
+
+	return 0;
+}
+
