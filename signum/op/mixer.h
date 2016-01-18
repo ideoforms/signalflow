c323fa00df7d1911d073ca77273fbd2745b24acc
Daniel Jones <daniel@jones.org.uk>
2016-01-18 20:34:57 +0000

    Implement Mixer unit for arbitrary mix-up/-down

--- signum/op/mixer.h
+++ signum/op/mixer.h
@@ -3,6 +3,7 @@
 #include "../constants.h"
 #include "../unit.h"
 #include "../registry.h"
+#include "../util.h"
 
 #include <list>
 
@@ -22,14 +23,40 @@ namespace signum
 
 		virtual void next(sample **out, int num_frames)
 		{
+			float out_channel_pan,
+				  in_channel_pan;
+
+			this->zero_output();
+
 			for (int out_channel = 0; out_channel < this->channels; out_channel++)
 			{
-				for (int frame = 0; frame < num_frames; frame++)
+				/*------------------------------------------------------------------------
+				 * out_channel_pan ranges from [0..1], where
+				 * leftmost channel = 0, rightmost channel = 1
+				 *-----------------------------------------------------------------------*/
+				if (this->channels > 1)
+					out_channel_pan = map(out_channel, 0, this->channels - 1, 0, 1);
+				else
+					out_channel_pan = 0.5;
+
+				for (int in_channel = 0; in_channel < this->channels_in; in_channel++)
 				{
-					out[out_channel][frame] = 0.0;
-					for (int channel = 0; channel < this->channels_in; channel++)
+
+					if (this->channels_in > 1)
+						in_channel_pan = map(in_channel, 0, this->channels_in - 1, 0, 1);
+					else
+						in_channel_pan = 0.5;
+
+					float channel_distance = fabs(in_channel_pan - out_channel_pan);
+					float channel_distance_max = 1.0 / (this->channels - 1);
+					float channel_amp = map(channel_distance,
+							channel_distance_max, 0,
+							0, 1);
+					channel_amp = clip(channel_amp, 0, 1);
+				
+					for (int frame = 0; frame < num_frames; frame++)
 					{
-						out[out_channel][frame] += this->input->out[channel][frame];
+						out[out_channel][frame] += channel_amp * this->input->out[in_channel][frame];
 					}
 				}
 			}
