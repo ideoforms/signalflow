88d18507135e772559189e6d11e3e7a0c19e9fc4
Daniel Jones <daniel@jones.org.uk>
2016-01-18 00:46:45 +0000

    Multiplex: Fix operation

--- signum/op/multiplex.h
+++ signum/op/multiplex.h
@@ -4,6 +4,8 @@
 #include "../unit.h"
 #include "../registry.h"
 
+#include <list>
+
 namespace signum
 {
 
@@ -19,14 +21,14 @@ namespace signum
 
 		virtual void next(sample **out, int num_frames)
 		{
-			int channel = 0; 
+			int global_channel = 0; 
 			for (UnitRef input : this->inputs)
 			{
-				for (int channel = 0; channel < input->channels_out; channel++)
+				for (int this_channel = 0; this_channel < input->channels_out; this_channel++)
 				{
-					memcpy(out[channel], input->out[channel], num_frames * sizeof(sample));
+					memcpy(out[global_channel + this_channel], input->out[this_channel], num_frames * sizeof(sample));
 				}
-				channel += input->channels_out;
+				global_channel += input->channels_out;
 			}
 		}
 
@@ -39,15 +41,21 @@ namespace signum
 			}
 
 			this->channels_out = this->channels_in;
-			// signum_debug("Unit %s set num_out_channels to %d", this->name.c_str(), this->channels_out);
+
+			this->min_input_channels = this->max_input_channels = this->channels_in;
+			this->min_output_channels = this->max_output_channels = this->channels_out;
+
+			signum_debug("Unit %s set num_out_channels to %d", this->name.c_str(), this->channels_out);
 		}
 
 		virtual void add_input(UnitRef input)
 		{
 			this->inputs.push_back(input);
+            std::string input_name = "input" + std::to_string(this->inputs.size());;
+            this->add_param(input_name, inputs.back());
 		}
 
-		std::vector <UnitRef> inputs;
+		std::list <UnitRef> inputs;
 	};
 
 	REGISTER(Multiplex, "multiplex");
