10cfa207a24d3293abc2a7bfddb852c7e27b7772
Daniel Jones <daniel@jones.org.uk>
2016-01-18 00:12:54 +0000

    Pan: Set channel config

--- signum/fx/pan.h
+++ signum/fx/pan.h
@@ -13,6 +13,9 @@ namespace signum
 				this->channels_in = 1;
 				this->channels_out = channels;
 
+				this->min_input_channels = this->max_input_channels = 1;
+				this->min_output_channels = this->max_output_channels = channels;
+
 				this->add_param("input", this->input);
 				this->add_param("pan", this->pan);
 
