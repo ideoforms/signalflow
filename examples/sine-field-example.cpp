4b036c7248a3e3706707c31af6027ddc28666501
Daniel Jones <daniel@jones.org.uk>
2016-01-18 22:58:16 +0000

    Improve sine field example

--- examples/sine-field-example.cpp
+++ examples/sine-field-example.cpp
@@ -11,17 +11,17 @@ int main()
 	 *-----------------------------------------------------------------------*/
 	for (int x = 0; x < 32; x++)
 	{
-		UnitRef sine = new Sine(rng_uniform(440, 880));
+		UnitRef sine = new Sine(rng_uniform(220, 1660));
 		UnitRef resample = new Resample(sine, 11025, 12);
 		UnitRef noise = new Noise(0.3, true, 1.0, 2);
 		UnitRef dust = new Dust(noise);
-		UnitRef env = new ASR(0.002, 0.05, 0.01);
+		UnitRef env = new ASR(0.005, 0.01, 0.05);
 		env->set_param("clock", dust);
 
 		UnitRef sum = resample * env;
-		UnitRef delay = new Delay(sum, 0.2, 0.4);
-		UnitRef pan = new Pan(2, delay * 0.1, rng_uniform(0, 1));
-		graph->output->add_input(pan);
+		UnitRef pan = new Pan(2, sum * 0.1, rng_uniform(0, 1));
+		UnitRef delay = new Delay(pan, 0.2, 0.4);
+		graph->output->add_input(delay);
 	}
 
 	graph->run();
