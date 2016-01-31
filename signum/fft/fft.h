9d4de417f4f513b25faddcdd0920adfcdf6a1008
Daniel Jones <daniel@jones.org.uk>
2016-01-31 17:49:28 +0000

    FFT/IFFT: Send and receive magnitude and phase values

--- signum/fft/fft.h
+++ signum/fft/fft.h
@@ -13,7 +13,7 @@ namespace signum
 	class FFT : public UnaryOpUnit
 	{
 		public:
-			FFT(UnitRef input = 0.0, int fft_size = 512) :
+			FFT(UnitRef input = 0.0, int fft_size = 1024) :
 				UnaryOpUnit(input), N(fft_size)
 			{
 				this->name = "fft";
@@ -22,6 +22,7 @@ namespace signum
 				this->fft_setup = vDSP_create_fftsetup(this->log2N, FFT_RADIX2);
 
 				this->buffer = (sample *) malloc(N * sizeof(sample));
+				this->buffer2 = (sample *) malloc(N * sizeof(sample));
 			}
 
 			~FFT()
@@ -34,6 +35,7 @@ namespace signum
 			int log2N;
 			FFTSetup fft_setup;
 			sample *buffer;
+			sample *buffer2;
 
 			virtual void next(sample **out, int num_frames)
 			{
@@ -45,18 +47,38 @@ namespace signum
 
 				DSPSplitComplex buffer_split = { buffer, buffer + N/2 };
 				DSPSplitComplex output_split = { out[0], out[0] + N/2 };
+				sample *magnitude = out[0];
+				sample *phase = out[0] + N/2;
 
 				/*------------------------------------------------------------------------
-				 * Convert from interleaved format (sequential pairs) to split format.
+				 * Convert from interleaved format (sequential pairs) to split format,
+				 * as required by the vDSP real-to-complex functions.
+				 * 
 				 * Stride of 2 is the smallest permissible value, denoting no gap
 				 * between "pairs" of samples".
 				 *-----------------------------------------------------------------------*/
-				vDSP_ctoz((DSPComplex *) input->out[0], 2, &buffer_split, 1, N / 2);
+				vDSP_ctoz((DSPComplex *) input->out[0], 2, &buffer_split, 1, N/2);
 
 				/*------------------------------------------------------------------------
-				 * Perform single-precision FFT.
+				 * Perform single-precision FFT, in-place (overwriting real values).
 				 *-----------------------------------------------------------------------*/
-				vDSP_fft_zrop(fft_setup, &buffer_split, 1, &output_split, 1, log2N, FFT_FORWARD);
+				vDSP_fft_zrip(fft_setup, &buffer_split, 1, log2N, FFT_FORWARD);
+
+				/*------------------------------------------------------------------------
+				 * Now, calculate magnitudes and phases, stored in our output buffer.
+				 *-----------------------------------------------------------------------*/
+
+				/*------------------------------------------------------------------------
+				 * 1. Sending cartesian values
+				 *-----------------------------------------------------------------------*/
+				// vDSP_ztoc(&buffer_split, 1, (DSPComplex *) out[0], 2, N/2);
+
+				/*------------------------------------------------------------------------
+				 * 2. Sending polar values
+				 *-----------------------------------------------------------------------*/
+				vDSP_ztoc(&buffer_split, 1, (DSPComplex *) buffer2, 2, N/2);
+				vDSP_polar(buffer2, 2, buffer, 2, N/2);
+				vDSP_ctoz((DSPComplex *) buffer, 2, &output_split, 1, N/2);
 			}
 	};
 
--- signum/fft/ifft.h
+++ signum/fft/ifft.h
@@ -12,7 +12,7 @@ namespace signum
 	class IFFT : public UnaryOpUnit
 	{
 		public:
-			IFFT(UnitRef input = 0.0, int fft_size = 512) :
+			IFFT(UnitRef input = 0.0, int fft_size = 1024) :
 				UnaryOpUnit(input), N(fft_size)
 			{
 				this->name = "ifft";
@@ -21,6 +21,7 @@ namespace signum
 				this->fft_setup = vDSP_create_fftsetup(this->log2N, FFT_RADIX2);
 
 				this->buffer = (sample *) malloc(N * sizeof(sample));
+				this->buffer2 = (sample *) malloc(N * sizeof(sample));
 			}
 
 			~IFFT()
@@ -32,17 +33,46 @@ namespace signum
 			int log2N;
 			FFTSetup fft_setup;
 			sample *buffer;
+			sample *buffer2;
 
 			virtual void next(sample **out, int num_frames)
 			{
-				// assert(num_frames == N);
+				assert(num_frames == N);
 
+				/*------------------------------------------------------------------------
+				 * Set up pointers to memory spaces so we can treat input and buffer
+				 * as split-valued complex pairs.
+				 *-----------------------------------------------------------------------*/
 				DSPSplitComplex buffer_split = { buffer, buffer + N/2 };
 				DSPSplitComplex input_split = { this->input->out[0], this->input->out[0] + N/2 };
 
-				vDSP_fft_zrop(fft_setup, &input_split, 1, &buffer_split, 1, log2N, FFT_INVERSE);
+				/*------------------------------------------------------------------------
+				 * Convert magnitude/phase to complex values.
+				 * Received values are split but vDSP_rect requires that pairs be
+				 * given sequentially, thus do a small dance.
+				 *-----------------------------------------------------------------------*/
+				vDSP_ztoc(&input_split, 1, (DSPComplex *) this->buffer, 2, N / 2);
+				vDSP_rect(this->buffer, 2, this->buffer2, 2, N / 2);
+
+				/*------------------------------------------------------------------------
+				 * 1. Expecting polar values
+				 *-----------------------------------------------------------------------*/
+				vDSP_ctoz((DSPComplex *) this->buffer2, 2, &buffer_split, 1, N / 2);
+
+				/*------------------------------------------------------------------------
+				 * 1. Expecting Cartesian values
+				 *-----------------------------------------------------------------------*/
+				// vDSP_ctoz((DSPComplex *) this->input->out[0], 2, &buffer_split, 1, N / 2);
+
+				/*------------------------------------------------------------------------
+				 * Perform inverse FFT
+				 *-----------------------------------------------------------------------*/
+				vDSP_fft_zrip(fft_setup, &buffer_split, 1, log2N, FFT_INVERSE);
 				vDSP_ztoc(&buffer_split, 1, (DSPComplex *) out[0], 2, N / 2);
 
+				/*------------------------------------------------------------------------
+				 * Scale down (Required by vDSP)
+				 *-----------------------------------------------------------------------*/
 				float scale = 1.0 / (N * 2.0);
 				vDSP_vsmul(out[0], 1, &scale, out[0], 1, N);
 			}
