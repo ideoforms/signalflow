0aac7842362a6969ebca980f06b1f9200b027543
Daniel Jones <daniel@jones.org.uk>
2016-03-21 22:35:51 +0000

    Split SynthDef into .h and .cpp

---
+++ signal/synthdef.cpp
@@ -0,0 +1,93 @@
+#include "synthdef.h"
+
+#include "core.h"
+#include "gen/constant.h"
+
+#include <iostream>
+
+namespace libsignal
+{
+			SynthDef::SynthDef(std::string name)
+			{
+				this->name = name;
+			}
+			
+			/*------------------------------------------------------------------------
+			 * TEMPLATING 
+			 *-----------------------------------------------------------------------*/
+
+			NodeRef SynthDef::add_input(std::string name, sample default_value)
+			{
+				NodeRef placeholder(default_value);
+				this->inputs[name] = placeholder.get();
+				return placeholder;
+			}
+
+			std::string SynthDef::get_input_name(const NodeRef &node)
+			{
+				for (auto input : this->inputs)
+				{
+					std::string name = input.first;
+					Node *node_ptr = input.second;
+					if (node_ptr == node.get())
+						return name;
+				}
+
+				return "";
+			}
+
+			NodeRef SynthDef::add_node(NodeRef node)
+			{
+				return node;
+			}
+
+			void SynthDef::set_output(const NodeRef &out)
+			{
+				this->output = out;
+			}
+
+			void SynthDef::save(std::string filename)
+			{
+			}
+
+			/*------------------------------------------------------------------------
+			 * Scans the graph structure beginning from its inputs and outputs.
+			 *-----------------------------------------------------------------------*/
+			NodeDefinition SynthDef::read_structure()
+			{
+				signal_assert(this->output != nullptr, "SynthDef %s: output is not set", this->name.c_str());
+				const NodeRef &r = this->output;
+				return this->read_structure(r);
+			}
+
+			NodeDefinition SynthDef::read_structure(const NodeRef &node)
+			{
+				NodeDefinition def(node->name);
+				if (node->name == "constant")
+				{
+					Constant *constant = (Constant *) node.get();
+					def.set_value(constant->value);
+				}
+				else
+				{
+					for (auto param : node->params)
+					{
+						NodeRef param_node = *(param.second);
+						if (param_node)
+						{
+							NodeDefinition param_def = this->read_structure(param_node);
+							def.add_param(param.first, &param_def);
+						}
+					}
+				}
+
+				std::string input_name = this->get_input_name(node);
+				if (!input_name.empty())
+				{
+					def.input_name = input_name;
+				}
+
+				return def;
+			}
+
+}
--- signal/synthdef.h
+++ signal/synthdef.h
@@ -1,97 +1,36 @@
+#include "node.h"
 #include "nodedef.h"
-
-#include "gen/constant.h"
-#include <iostream>
 
 namespace libsignal
 {
 	class SynthDef
 	{
 		public:
-			SynthDef(std::string name)
-			{
-				this->name = name;
-			}
+			SynthDef(std::string name);
+			
+			/*----------------------------------------------------------------------------------
+			 * Methods for creating a SynthDef from live Node objects.
+			 *---------------------------------------------------------------------------------*/
+			NodeRef add_input(std::string name, sample default_value);
+			NodeRef add_node(NodeRef node);
+			void set_output(const NodeRef &out);
 
-			NodeRef add_input(std::string name, sample default_value)
-			{
-				NodeRef placeholder(default_value);
-				this->inputs[name] = placeholder.get();
-				return placeholder;
-			}
+			/*----------------------------------------------------------------------------------
+			 * Save a SynthDef to disk.
+			 *---------------------------------------------------------------------------------*/
+			void save(std::string filename);
 
-			std::string get_input_name(const NodeRef &node)
-			{
-				for (auto input : this->inputs)
-				{
-					std::string name = input.first;
-					Node *node_ptr = input.second;
-					if (node_ptr == node.get())
-						return name;
-				}
-
-				return "";
-			}
-
-			NodeRef add_node(NodeRef node)
-			{
-				return node;
-			}
-
-			void set_output(const NodeRef &out)
-			{
-				this->output = out;
-			}
-
-			void save(std::string filename)
-			{
-			}
-
-			/*------------------------------------------------------------------------
-			 * Scans the graph structure beginning from its inputs and outputs.
-			 *-----------------------------------------------------------------------*/
-			NodeDefinition read_structure()
-			{
-				signal_assert(this->output != nullptr, "SynthDef %s: output is not set", this->name.c_str());
-				const NodeRef &r = this->output;
-				return this->read_structure(r);
-			}
-
-			NodeDefinition read_structure(const NodeRef &node)
-			{
-				NodeDefinition def(node->name);
-				if (node->name == "constant")
-				{
-					Constant *constant = (Constant *) node.get();
-					def.set_value(constant->value);
-				}
-				else
-				{
-					for (auto param : node->params)
-					{
-						NodeRef param_node = *(param.second);
-						if (param_node)
-						{
-							NodeDefinition param_def = this->read_structure(param_node);
-							def.add_param(param.first, &param_def);
-						}
-					}
-				}
-
-				std::string input_name = this->get_input_name(node);
-				if (!input_name.empty())
-				{
-					def.input_name = input_name;
-				}
-
-				return def;
-			}
+			/*----------------------------------------------------------------------------------
+			 * 
+			 *---------------------------------------------------------------------------------*/
+			NodeDefinition read_structure();
 
 			std::string name;
-			std::unordered_map <std::string, Node *> inputs;
 			NodeRef output = nullptr;
 
-			std::vector <NodeDefinition> nodedefs;
+		private:
+			std::string get_input_name(const NodeRef &node);
+			NodeDefinition read_structure(const NodeRef &node);
+			std::unordered_map <std::string, Node *> inputs;
 	};
-
 }
