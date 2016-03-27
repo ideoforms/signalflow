c7efb84c41030def9c6d22546931f34a25057d9d
Daniel Jones <daniel@jones.org.uk>
2016-03-27 17:31:51 +0100

    Update Square to handle arbitrary input channels

--- signal/gen/square.h
+++ signal/gen/square.h
@@ -7,16 +7,17 @@ namespace libsignal
 	class Square : public Node
 	{
 	public:
-		Square(NodeRef frequency, NodeRef width = 0.5) : frequency(frequency), width(width), phase(0)
+		Square(NodeRef frequency, NodeRef width = 0.5) : frequency(frequency), width(width)
 		{
 			this->add_param("frequency", this->frequency);
 			this->add_param("width", this->width);
+			memset(this->phase, 0, sizeof(this->phase));
 		};
 
 		NodeRef frequency;
 		NodeRef width;
 
-		float phase;
+		float phase[SIGNAL_MAX_CHANNELS];
 
 		virtual void next(sample **out, int num_frames);
 	};
