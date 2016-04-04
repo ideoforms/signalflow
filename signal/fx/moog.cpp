ecf4e8083dd6f280e17ec3063a5c426d5bd12f9b
Daniel Jones <daniel@jones.org.uk>
2016-04-05 00:02:59 +0100

    MoogVCF: Support for multichannel input

--- signal/fx/moog.cpp
+++ signal/fx/moog.cpp
@@ -9,7 +9,8 @@ namespace libsignal
 
 void MoogVCF::next(sample **out, int num_frames)
 {
-	int channel = 0;
+	for (int channel = 0; channel < num_output_channels; channel++)
+	{
 		for (int frame = 0; frame < num_frames; frame++)
 		{
 			float cutoff = this->cutoff->out[channel][frame];
@@ -22,19 +23,20 @@ void MoogVCF::next(sample **out, int num_frames)
 			 *-----------------------------------------------------------------------*/
 			float input = this->input->out[channel][frame];
 
-			input -= out4 * fb;
+			input -= out4[channel] * fb;
 			input *= 0.35013 * (f*f)*(f*f);
-			out1 = input + 0.3 * in1 + (1 - f) * out1; // Pole 1
-			in1  = input;
-			out2 = out1 + 0.3 * in2 + (1 - f) * out2;  // Pole 2
-			in2  = out1;
-			out3 = out2 + 0.3 * in3 + (1 - f) * out3;  // Pole 3
-			in3  = out2;
-			out4 = out3 + 0.3 * in4 + (1 - f) * out4;  // Pole 4
-			in4  = out3;
+			out1[channel] = input + 0.3 * in1[channel] + (1 - f) * out1[channel]; // Pole 1
+			in1[channel]  = input;
+			out2[channel] = out1[channel] + 0.3 * in2[channel] + (1 - f) * out2[channel];  // Pole 2
+			in2[channel]  = out1[channel];
+			out3[channel] = out2[channel] + 0.3 * in3[channel] + (1 - f) * out3[channel];  // Pole 3
+			in3[channel]  = out2[channel];
+			out4[channel] = out3[channel] + 0.3 * in4[channel] + (1 - f) * out4[channel];  // Pole 4
+			in4[channel]  = out3[channel];
 
-			this->out[channel][frame] = out4;
+			this->out[channel][frame] = out4[channel];
 		}
+	}
 }
 
 }
--- signal/fx/moog.h
+++ signal/fx/moog.h
@@ -16,13 +16,27 @@ namespace libsignal
 				this->add_param("resonance", this->resonance);
 
 				int int_buf_size = SIGNAL_MAX_CHANNELS * sizeof(int);
+				memset(out1, 0, int_buf_size);
+				memset(out2, 0, int_buf_size);
+				memset(out3, 0, int_buf_size);
+				memset(out4, 0, int_buf_size);
+				memset(in1, 0, int_buf_size);
+				memset(in2, 0, int_buf_size);
+				memset(in3, 0, int_buf_size);
+				memset(in4, 0, int_buf_size);
 			}
 
 			NodeRef cutoff;
 			NodeRef resonance;
 
-			float out1 = 0, out2 = 0, out3 = 0, out4 = 0;
-			float in1 = 0, in2 = 0, in3 = 0, in4 = 0;
+			float out1[SIGNAL_MAX_CHANNELS],
+				  out2[SIGNAL_MAX_CHANNELS],
+				  out3[SIGNAL_MAX_CHANNELS],
+				  out4[SIGNAL_MAX_CHANNELS];
+			float in1[SIGNAL_MAX_CHANNELS],
+				  in2[SIGNAL_MAX_CHANNELS],
+				  in3[SIGNAL_MAX_CHANNELS],
+				  in4[SIGNAL_MAX_CHANNELS];
 
 			virtual void next(sample **out, int num_frames);
 	};
