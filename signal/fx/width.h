9bf225507ccb3ecf8d4b8751dc3314a55333e66c
Daniel Jones <daniel@jones.org.uk>
2016-04-04 21:14:59 +0100

    Fix Width node

--- signal/fx/width.h
+++ signal/fx/width.h
@@ -4,17 +4,18 @@
 
 namespace libsignal
 {
-	class Width : public Node
+	class Width : public UnaryOpNode
 	{
 		public:
-			Width(NodeRef input = 0, NodeRef width = 1) :
-				input(input), width(width)
+			Width(NodeRef input = 0, NodeRef width = 1) : UnaryOpNode(input), width(width)
 			{
-				this->num_input_channels = 1;
+				this->name = "width";
+				this->add_param("width", this->width);
+
+				this->num_input_channels = 2;
 				this->num_output_channels = 2;
 			}
 
-			NodeRef input;
 			NodeRef width;
 
 			virtual void next(sample **out, int num_frames);
