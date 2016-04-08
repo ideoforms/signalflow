30eb0253d98f1e85843e4a9a2b5a473f968adbd3
Daniel Jones <daniel@jones.org.uk>
2016-04-08 10:07:41 +0100

    MoogVCF: Add scaling from frequency to normalised values

--- signal/fx/moog.cpp
+++ signal/fx/moog.cpp
@@ -13,25 +13,41 @@ void MoogVCF::next(sample **out, int num_frames)
 	{
 		for (int frame = 0; frame < num_frames; frame++)
 		{
-			float cutoff = this->cutoff->out[channel][frame];
+			float cutoff = map(this->cutoff->out[channel][frame], 0, this->graph->sample_rate / 2, 0.005, 1);
 			float resonance = this->resonance->out[channel][frame];
 			float f = cutoff * 1.16; 
 			float fb = resonance * (1.0 - 0.15 * f * f);
 
 			/*------------------------------------------------------------------------
-			 * Low-pass filter
+			 * Calculate filter
 			 *-----------------------------------------------------------------------*/
 			float input = this->input->out[channel][frame];
 
 			input -= out4[channel] * fb;
-			input *= 0.35013 * (f*f)*(f*f);
-			out1[channel] = input + 0.3 * in1[channel] + (1 - f) * out1[channel]; // Pole 1
+			input *= 0.35013 * f*f*f*f;
+
+			/*------------------------------------------------------------------------
+			 * Pole 1
+			 *-----------------------------------------------------------------------*/
+			out1[channel] = input + 0.3 * in1[channel] + (1 - f) * out1[channel];
 			in1[channel]  = input;
-			out2[channel] = out1[channel] + 0.3 * in2[channel] + (1 - f) * out2[channel];  // Pole 2
+
+			/*------------------------------------------------------------------------
+			 * Pole 2
+			 *-----------------------------------------------------------------------*/
+			out2[channel] = out1[channel] + 0.3 * in2[channel] + (1 - f) * out2[channel];
 			in2[channel]  = out1[channel];
-			out3[channel] = out2[channel] + 0.3 * in3[channel] + (1 - f) * out3[channel];  // Pole 3
+
+			/*------------------------------------------------------------------------
+			 * Pole 3
+			 *-----------------------------------------------------------------------*/
+			out3[channel] = out2[channel] + 0.3 * in3[channel] + (1 - f) * out3[channel];
 			in3[channel]  = out2[channel];
-			out4[channel] = out3[channel] + 0.3 * in4[channel] + (1 - f) * out4[channel];  // Pole 4
+
+			/*------------------------------------------------------------------------
+			 * Pole 4
+			 *-----------------------------------------------------------------------*/
+			out4[channel] = out3[channel] + 0.3 * in4[channel] + (1 - f) * out4[channel];
 			in4[channel]  = out3[channel];
 
 			this->out[channel][frame] = out4[channel];
