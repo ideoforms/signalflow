60c618bcaa1c830ace9f9774e2b4aed8bc7a1e59
Daniel Jones <daniel@jones.org.uk>
2016-04-19 08:54:14 +0100

    GraphRenderer: Add comments

--- signal/renderer.cpp
+++ signal/renderer.cpp
@@ -12,23 +12,35 @@ GraphRenderer::GraphRenderer()
 
 void GraphRenderer::render_node(NodeRef node)
 {
+	/*------------------------------------------------------------------------
+	 * Don't render nodes repeatedly
+	 *-----------------------------------------------------------------------*/
 	if (this->rendered_nodes.find(node.get()) != this->rendered_nodes.end())
 		return;
 	this->rendered_nodes.insert(node.get());
 
 	if (node->name == "constant")
 	{
+		/*------------------------------------------------------------------------
+		 * Render constant numeric values in a ring
+		 *-----------------------------------------------------------------------*/
 		nodestream << "\"" << (void const *) node.get() << "\" [shape=circle, label = \"";
 		nodestream << ((Constant *) node.get())->value;
 		nodestream << "\"]; ";
 	}
 	else
 	{
+		/*------------------------------------------------------------------------
+		 * Render Node names in a box
+		 *-----------------------------------------------------------------------*/
 		nodestream << "\"" << (void const *) node.get() << "\" [fontname=\"helvetica-bold\", label = \"";
 		nodestream << node->name;
 		nodestream << "\"]; ";
 	}
 
+	/*------------------------------------------------------------------------
+	 * Collate edges, titled based on their node input
+	 *-----------------------------------------------------------------------*/
 	for (auto param : node->params)
 	{
 		NodeRef value = *(param.second);
@@ -42,6 +54,9 @@ void GraphRenderer::render_node(NodeRef node)
 
 void GraphRenderer::render(GraphRef graph)
 {
+	/*------------------------------------------------------------------------
+	 * Recurse and render the complete output
+	 *-----------------------------------------------------------------------*/
 	this->rendered_nodes.clear();
 	this->render_node(graph->output);
 	std::cout << "digraph { splines=ortho; graph [pad=1, ranksep=0.5, nodesep=0.5]; node [ fontname = helvetica, fontsize = 11, shape = box  ]; edge [ fontname = helvetica, fontsize = 9 ]; " << nodestream.str() << edgestream.str() << "} " << std::endl;
