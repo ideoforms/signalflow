3f85c608bc7408e49398a86f7e982fbd8a08df40
Daniel Jones <daniel@jones.org.uk>
2016-04-24 19:08:28 +0100

    Buffer: Save to wav

--- signal/buffer.cpp
+++ signal/buffer.cpp
@@ -79,6 +79,56 @@ void Buffer::open(const char *filename)
 	this->sample_rate = info.samplerate;
 	this->duration = this->num_frames / this->sample_rate;
 
+	sf_close(sndfile);
+
+	#endif
+}
+
+void Buffer::save(const char *filename)
+{
+	#ifdef HAVE_SNDFILE
+
+    SF_INFO info;
+	memset(&info, 0, sizeof(SF_INFO));
+	info.frames = this->num_frames;
+	info.channels = this->num_channels;
+	info.samplerate = (int) this->sample_rate;
+	info.format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
+    SNDFILE *sndfile = sf_open(filename, SFM_WRITE, &info);
+
+    if (!sndfile)
+    {
+        printf("Failed to write soundfile (%d)\n", sf_error(NULL));
+        exit(1);
+    }
+
+	int frames_per_write = 1024;
+	int samples_per_write = frames_per_write * info.channels;
+	sample buffer[samples_per_write];
+    int frame_index = 0;
+
+    while (true)
+    {
+		int frames_this_write = frames_per_write;
+		if (this->num_frames - frame_index < frames_this_write)
+			frames_this_write = this->num_frames - frame_index;
+
+		for (int frame = 0; frame < frames_this_write; frame++)
+		{
+			// TODO: Vector-accelerated interleave
+			for (int channel = 0; channel < info.channels; channel++)
+			{
+				buffer[frame * info.channels + channel] = this->data[channel][frame_index];
+			}
+			frame_index++;
+		}
+        int count = sf_writef_float(sndfile, buffer, frames_this_write);
+        if (frame_index >= this->num_frames)
+            break;
+    }
+
+	sf_close(sndfile);
+
 	#endif
 }
 
