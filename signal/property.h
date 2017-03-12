6410cdd69687b02671f44329c99a38460c4358cf
Daniel Jones <daniel@jones.org.uk>
2017-03-12 14:44:00 +0000

    Property: Make values mutable

--- signal/property.h
+++ signal/property.h
@@ -12,6 +12,12 @@ class Property
 		virtual std::string string_value() { return ""; }
 		virtual std::vector <float> float_array_value() { return std::vector <float> (); }
 		virtual std::vector <std::string> string_array_value() { return std::vector <std::string> (); }
+
+		virtual void set_int_value(int value) { }
+		virtual void set_float_value(float value) { }
+		virtual void set_string_value(std::string value) { }
+		virtual void set_float_array_value(std::vector <float> value) { }
+		virtual void set_string_array_value(std::vector <std::string> value) { }
 };
 
 template <typename T>
@@ -19,10 +25,10 @@ class TypedProperty : public Property
 {
 	public:
 		TypedProperty (const T &value) : value(value) { }
-		T get_value() { return value; }
+		void set(T new_value) { value = new_value; }
 
 	protected:
-		const T value;
+		T value;
 };
 
 class FloatProperty : public TypedProperty <float>
@@ -31,6 +37,8 @@ class FloatProperty : public TypedProperty <float>
 		FloatProperty(float value) : TypedProperty(value) {}
 		virtual int int_value() override { return value; }
 		virtual float float_value() override { return value; }
+		virtual void set_int_value(int new_value) override { value = new_value; }
+		virtual void set_float_value(float new_value) override { value = new_value; }
 };
 
 class IntProperty : public TypedProperty <int>
@@ -39,27 +47,32 @@ class IntProperty : public TypedProperty <int>
 		IntProperty(int value) : TypedProperty(value) {}
 		virtual int int_value() override { return value; }
 		virtual float float_value() override { return value; }
+		virtual void set_int_value(int new_value) override { value = new_value; }
+		virtual void set_float_value(float new_value) override { value = new_value; }
 };
 
 class StringProperty : public TypedProperty <std::string>
 {
 	public:
 		StringProperty(std::string value) : TypedProperty(value) {}
-		virtual std::string string_value() { return value; }
+		virtual std::string string_value() override { return value; }
+		virtual void set_string_value(std::string new_value) override { value = new_value; }
 };
 
 class FloatArrayProperty : public TypedProperty <std::vector <float>>
 {
 	public:
 		FloatArrayProperty(std::vector <float> value) : TypedProperty(value) {}
-		virtual std::vector <float> float_array_value() { return value; }
+		virtual std::vector <float> float_array_value() override { return value; }
+		virtual void set_float_array_value(std::vector <float> new_value) override { value = new_value; }
 };
 
 class StringArrayProperty : public TypedProperty <std::vector <std::string>>
 {
 	public:
 		StringArrayProperty(std::vector <std::string> value) : TypedProperty(value) {}
-		virtual std::vector <std::string> string_array_value() { return value; }
+		virtual std::vector <std::string> string_array_value() override { return value; }
+		virtual void set_string_array_value(std::vector <std::string> new_value) override { value = new_value; }
 };
 
 
@@ -78,5 +91,7 @@ class PropertyRef : public std::shared_ptr<Property>
 		PropertyRef(std::vector <float> value) : std::shared_ptr<Property>(new FloatArrayProperty(value)) { }
 		PropertyRef(std::initializer_list <std::string> value) : std::shared_ptr<Property>(new StringArrayProperty(value)) { }
 		PropertyRef(std::vector <std::string> value) : std::shared_ptr<Property>(new StringArrayProperty(value)) { }
+
+		// void set_value(int value) { this->get()->set_value(value); }
 };
 
