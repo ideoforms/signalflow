331f75c12c7785c659f4a90064c8ed3d9642fca0
Daniel Jones <daniel@jones.org.uk>
2018-12-27 17:41:51 +0000

    Envelope: tabs -> spaces

--- signal/envelope/env.cpp
+++ signal/envelope/env.cpp
@@ -5,73 +5,73 @@ namespace libsignal
 {
 
 ASR::ASR(NodeRef attack, NodeRef sustain, NodeRef release, NodeRef clock) :
-	attack(attack), sustain(sustain), release(release), clock(clock)
+    attack(attack), sustain(sustain), release(release), clock(clock)
 {
-	this->phase = 0.0;
+    this->phase = 0.0;
 
-	this->name = "env-asr";
-	this->add_input("clock", this->clock);
-	this->add_input("attack", this->attack);
-	this->add_input("sustain", this->sustain);
-	this->add_input("release", this->release);
+    this->name = "env-asr";
+    this->add_input("clock", this->clock);
+    this->add_input("attack", this->attack);
+    this->add_input("sustain", this->sustain);
+    this->add_input("release", this->release);
 }
 
 void ASR::trigger(std::string name, float value)
 {
-	if (name == SIGNAL_DEFAULT_TRIGGER)
-	{
-		this->phase = 0.0;
-	}
+    if (name == SIGNAL_DEFAULT_TRIGGER)
+    {
+        this->phase = 0.0;
+    }
 }
 
 void ASR::process(sample **out, int num_frames)
 {
-	sample rv;
+    sample rv;
 
-	for (int frame = 0; frame < num_frames; frame++)
-	{
-		SIGNAL_PROCESS_TRIGGER(this->clock, frame, SIGNAL_DEFAULT_TRIGGER);
+    for (int frame = 0; frame < num_frames; frame++)
+    {
+        SIGNAL_PROCESS_TRIGGER(this->clock, frame, SIGNAL_DEFAULT_TRIGGER);
 
-		float attack = this->attack->out[0][frame];
-		float sustain = this->sustain->out[0][frame];
-		float release = this->release->out[0][frame];
+        float attack = this->attack->out[0][frame];
+        float sustain = this->sustain->out[0][frame];
+        float release = this->release->out[0][frame];
 
-		if (this->phase < attack)
-		{
-			/*------------------------------------------------------------------------
-			 * Attack phase.
-			 *-----------------------------------------------------------------------*/
-			rv = (this->phase / attack);
-		}
-		else if (this->phase <= attack + sustain)
-		{
-			/*------------------------------------------------------------------------
-			 * Sustain phase.
-			 *-----------------------------------------------------------------------*/
-			rv = 1.0;
-		}
-		else if (this->phase < attack + sustain + release)
-		{
-			/*------------------------------------------------------------------------
-			 * Release phase.
-			 *-----------------------------------------------------------------------*/
-			rv = 1.0 - (this->phase - (attack + sustain)) / release;
-		}
-		else
-		{
-			/*------------------------------------------------------------------------
-			 * Envelope has finished.
-			 *-----------------------------------------------------------------------*/
-			rv = 0.0;
-		}
+        if (this->phase < attack)
+        {
+            /*------------------------------------------------------------------------
+             * Attack phase.
+             *-----------------------------------------------------------------------*/
+            rv = (this->phase / attack);
+        }
+        else if (this->phase <= attack + sustain)
+        {
+            /*------------------------------------------------------------------------
+             * Sustain phase.
+             *-----------------------------------------------------------------------*/
+            rv = 1.0;
+        }
+        else if (this->phase < attack + sustain + release)
+        {
+            /*------------------------------------------------------------------------
+             * Release phase.
+             *-----------------------------------------------------------------------*/
+            rv = 1.0 - (this->phase - (attack + sustain)) / release;
+        }
+        else
+        {
+            /*------------------------------------------------------------------------
+             * Envelope has finished.
+             *-----------------------------------------------------------------------*/
+            rv = 0.0;
+        }
 
-		this->phase += 1.0 / this->graph->sample_rate;
+        this->phase += 1.0 / this->graph->sample_rate;
 
-		for (int channel = 0; channel < this->num_output_channels; channel++)
-		{
-			out[channel][frame] = rv;
-		}
-	}
+        for (int channel = 0; channel < this->num_output_channels; channel++)
+        {
+            out[channel][frame] = rv;
+        }
+    }
 }
 
 }
