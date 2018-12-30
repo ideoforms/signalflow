2c0873cabdfeb877f4c4e6ed60f89daa6a936d91
Daniel Jones <daniel@jones.org.uk>
2018-12-30 12:49:23 +0000

    Monitor: Add support for multichannel monitoring

--- signal/monitor.cpp
+++ signal/monitor.cpp
@@ -23,9 +23,27 @@ void NodeMonitor::start()
 void NodeMonitor::run_thread()
 {
     float sleep_time = 1000000.0 * (1.0 / this->frequency);
+    char buffer[1024];
     while (this->running)
     {
-        fprintf(stderr, "%s: %.5f\n", this->label.c_str(), this->node->out[0][0]);
+        if (this->node->num_output_channels > 1)
+        {
+            strcpy(buffer, "{ ");
+            for (uint8_t i = 0; i < this->node->num_output_channels; i++)
+            {
+                sprintf(buffer + strlen(buffer), "%.5f", this->node->out[i][0]);
+                if (i < this->node->num_output_channels - 1)
+                {
+                    sprintf(buffer + strlen(buffer), ", ");
+                }
+            }
+            strcat(buffer, " }");
+            fprintf(stderr, "%s: %s\n", this->label.c_str(), buffer);
+        }
+        else
+        {
+            fprintf(stderr, "%s: %.5f\n", this->label.c_str(), this->node->out[0][0]);
+        }
         usleep(sleep_time);
     }
 }
