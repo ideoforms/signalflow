a3472984571e96e9bce53796881c87fa8358c06d
Daniel Jones <daniel@jones.org.uk>
2020-04-19 15:25:09 +0100

    Improved wavetable example

--- examples/wavetable-example.cpp
+++ examples/wavetable-example.cpp
@@ -5,6 +5,7 @@
  * waveforms from a fixed audio buffer.
  *-----------------------------------------------------------------------*/
 #include <signal/signal.h>
+#include <math.h>
 
 using namespace libsignal;
 
@@ -12,9 +13,17 @@ int main()
 {
     AudioGraphRef graph = new AudioGraph();
 
-    BufferRef buffer = new EnvelopeBufferHanning(256);
     NodeRef freq = new Noise(2.0, true, 140, 800);
     freq = new RoundToScale(freq);
+
+    int wavetable_size = 4096;
+    sample **wt = new sample*[1];
+    wt[0] = new sample[wavetable_size];
+    for (int i = 0; i < wavetable_size; i++)
+    {
+        wt[0][i] = sin(i * M_PI * 2 / wavetable_size);
+    }
+    BufferRef buffer = new Buffer(1, wavetable_size, wt);
     NodeRef wavetable = new Wavetable(buffer, freq * 2.0);
     NodeRef delay = new Delay(wavetable, 0.1, 0.3);
 
