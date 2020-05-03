00cd06369079482d4e90d9e082c0d5db7618b3e9
Daniel Jones <daniel@jones.org.uk>
2020-05-03 21:44:22 +0100

    Add support for wait with timeout

--- source/src/python/graph.cpp
+++ source/src/python/graph.cpp
@@ -16,9 +16,32 @@ void init_python_graph(py::module &m)
         {
             // Interruptible wait
             // https://pybind11.readthedocs.io/en/stable/faq.html#how-can-i-properly-handle-ctrl-c-in-long-running-functions
-            for (;;) {
+            for (;;)
+            {
                 if (PyErr_CheckSignals() != 0)
                     throw py::error_already_set();
+            }
+        })
+        .def("wait", [](AudioGraph &graph, float timeout_seconds)
+        {
+            timeval tv;
+            gettimeofday(&tv, NULL);
+            double t0 = tv.tv_sec + tv.tv_usec / 1000000.0;
+
+            for (;;)
+            {
+                if (PyErr_CheckSignals() != 0)
+                    throw py::error_already_set();
+
+                if (timeout_seconds)
+                {
+                    gettimeofday(&tv, NULL);
+                    double t1 = tv.tv_sec + tv.tv_usec / 1000000.0;
+                    if (t1 - t0 > timeout_seconds)
+                    {
+                        break;
+                    }
+                }
             }
         })
         .def("add_output", [](AudioGraph &graph, NodeRef a) { graph.add_output(a); })
