d9b3cbbe92f846dcb708c4c1571794dd931eff07
Daniel Jones <daniel@jones.org.uk>
2020-05-04 18:34:02 +0100

    More Buffer unit tests

--- tests/test_buffer.py
+++ tests/test_buffer.py
@@ -15,7 +15,7 @@ def test_buffer():
     assert b.num_frames == len(data[0])
     assert np.array_equal(data, b.data)
 
-def test_buffer_interpolation():
+def test_buffer_interpolate():
     b = Buffer(1, 2, np.array([[ 1, 2 ]]))
     assert b.get(0) == 1
     assert b.get(1) == 2
@@ -30,6 +30,23 @@ def test_buffer_interpolation():
     assert b.get(0.5) == 1
     assert b.get(0.25) == 1
 
+def test_buffer_fill():
+    b = Buffer(4, 44100)
+    b.fill(0.5)
+    assert np.all(b.data[0] == 0.5)
+    assert np.all(b.data[1] == 0.5)
+    assert np.all(b.data[2] == 0.5)
+    assert np.all(b.data[3] == 0.5)
+
+def test_buffer_split():
+    b = Buffer(1, 8192)
+    b.fill(1)
+    buffers = b.split(2048)
+    for buf in buffers:
+        assert buf.num_channels == 1
+        assert buf.num_frames == 2048
+        assert np.all(b.data[0] == 1)
+
 def test_buffer_load():
     b = Buffer("examples/audio/gliss.aif")
     assert b.num_frames == 262856
@@ -43,10 +60,3 @@ def test_buffer_save():
     b = Buffer(1, 44100)
     # TODO
 
-def test_buffer_fill():
-    b = Buffer(4, 44100)
-    b.fill(0.5)
-    assert np.all(b.data[0] == 0.5)
-    assert np.all(b.data[1] == 0.5)
-    assert np.all(b.data[2] == 0.5)
-    assert np.all(b.data[3] == 0.5)
