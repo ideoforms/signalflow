30d9963f0dd41b8e0aa7fe2d30dff383ed5e2789
Daniel Jones <daniel@jones.org.uk>
2020-05-05 23:39:35 +0100

    Fix dud tests

--- tests/test_nodes_stochastic.py
+++ tests/test_nodes_stochastic.py
@@ -10,12 +10,10 @@ def test_random_impulse(graph):
     b = Buffer(1, graph.sample_rate)
     a = RandomImpulse(frequency)
     process_tree(a, buffer=b)
-    impulses = np.sum(a.output_buffer[0])
+    impulses = np.sum(b.data[0])
     assert (impulses - frequency) < (0.1 * frequency)
-    assert np.sum(a.output_buffer[1]) == 0
 
     a = RandomImpulse(frequency, SIGNAL_EVENT_DISTRIBUTION_POISSON)
     process_tree(a, buffer=b)
-    impulses = np.sum(a.output_buffer[0])
+    impulses = np.sum(b.data[0])
     assert (impulses - frequency) < (0.1 * frequency)
-    assert np.sum(a.output_buffer[1]) == 0
