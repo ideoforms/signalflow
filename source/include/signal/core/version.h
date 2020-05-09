2ecd1f2196b677ea7299c8d8e6655e09a66b3812
Daniel Jones <daniel@jones.org.uk>
2020-05-09 22:20:47 +0100

    Remove holders.h

--- source/include/signal/core/version.h
+++ source/include/signal/core/version.h
@@ -1,2 +1,2 @@
 #define SIGNAL_VERSION "0.3.1"
-#define SIGNAL_BUILD 737
+#define SIGNAL_BUILD 738
--- source/include/signal/holders.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Daniel Jones on 09/05/2020.
-//
-
-#ifndef SIGNAL_HOLDERS_H
-#define SIGNAL_HOLDERS_H
-
-#endif //SIGNAL_HOLDERS_H
