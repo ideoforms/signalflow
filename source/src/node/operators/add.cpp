a651f96827cee1f76aa7121362af3ee1db431409
Daniel Jones <daniel@jones.org.uk>
2020-05-10 09:55:44 +0100

    Integrate vDSP add

--- source/src/node/operators/add.cpp
+++ source/src/node/operators/add.cpp
@@ -11,12 +11,20 @@ Add::Add(NodeRef a, NodeRef b)
 
 void Add::process(sample **out, int num_frames)
 {
-    for (int frame = 0; frame < num_frames; frame++)
+    for (int channel = 0; channel < this->num_output_channels; channel++)
     {
-        for (int channel = 0; channel < this->num_output_channels; channel++)
+#ifdef __APPLE__
+
+        vDSP_vadd(input0->out[channel], 1, input1->out[channel], 1, out[channel], 1, num_frames);
+
+#else
+
+        for (int frame = 0; frame < num_frames; frame++)
         {
             out[channel][frame] = input0->out[channel][frame] + input1->out[channel][frame];
         }
+
+#endif
     }
 }
 
