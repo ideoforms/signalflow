fd9fd285b30845b54bea293f5504925733e02b1e
Daniel Jones <daniel@jones.org.uk>
2020-05-12 22:18:18 +0100

    test_graph_sample_rate

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,6 +1,20 @@
-import libsignal
+from libsignal import AudioGraph, Buffer, Sine
+from . import process_tree, count_zero_crossings
 
 def test_graph():
-    graph = libsignal.AudioGraph()
+    graph = AudioGraph()
     assert graph
+    del graph
+
+def test_graph_sample_rate():
+    graph = AudioGraph()
+    assert graph.sample_rate > 0
+    graph.sample_rate = 1000
+    assert graph.sample_rate == 1000
+
+    buf = Buffer(1, 1000)
+    sine = Sine(10)
+    process_tree(sine, buf)
+    assert count_zero_crossings(buf.data[0]) == 10
+    
     del graph
