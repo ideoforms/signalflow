04f803f663d1c9bbf4fc807f2174fbf802fb1ae5
Daniel Jones <daniel@jones.org.uk>
2020-05-16 13:13:13 +0100

    Add DelayOneTap

---
+++ source/src/node/filters/delays/onetap.cpp
@@ -0,0 +1,46 @@
+#include "signal/node/filters/delays/onetap.h"
+#include "signal/core/graph.h"
+#include "signal/node/oscillators/constant.h"
+
+namespace libsignal
+{
+
+OneTapDelay::OneTapDelay(NodeRef input, NodeRef delaytime, float maxdelaytime)
+    : UnaryOpNode(input), delaytime(delaytime), maxdelaytime(maxdelaytime)
+{
+    this->name = "one-tap-delay";
+    this->add_input("delay_time", this->delaytime);
+
+    SIGNAL_CHECK_GRAPH();
+    for (int i = 0; i < SIGNAL_MAX_CHANNELS; i++)
+    {
+        buffers.push_back(new SampleRingBuffer(maxdelaytime * this->graph->get_sample_rate()));
+    }
+}
+
+OneTapDelay::~OneTapDelay()
+{
+    for (auto buffer : buffers)
+    {
+        delete buffer;
+    }
+}
+
+void OneTapDelay::process(sample **out, int num_frames)
+{
+    SIGNAL_CHECK_GRAPH();
+
+    for (int channel = 0; channel < this->num_input_channels; channel++)
+    {
+        for (int frame = 0; frame < num_frames; frame++)
+        {
+            sample delay = this->delaytime->out[channel][frame];
+            float offset = delay * this->graph->get_sample_rate();
+
+            out[channel][frame] = buffers[channel]->get(-offset);
+            buffers[channel]->append(this->input->out[channel][frame]);
+        }
+    }
+}
+
+}
