9bc30beb573d5efd93e38e470ec224e2e99786c9
Daniel Jones <daniel@jones.org.uk>
2020-06-24 21:51:02 +0100

    Fix tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,12 +1,12 @@
-from libsignal import signal_clip
+import libsignal
 import pytest
 
 def test_clip():
-    assert signal_clip(-0.5, 0, 1) == 0.0
-    assert signal_clip(0.0, 0, 1) == 0.0
-    assert signal_clip(0.5, 0, 1) == 0.5
-    assert signal_clip(1.0, 0, 1) == 1.0
-    assert signal_clip(1.5, 0, 1) == 1.0
+    assert libsignal.clip(-0.5, 0, 1) == 0.0
+    assert libsignal.clip(0.0, 0, 1) == 0.0
+    assert libsignal.clip(0.5, 0, 1) == 0.5
+    assert libsignal.clip(1.0, 0, 1) == 1.0
+    assert libsignal.clip(1.5, 0, 1) == 1.0
 
 @pytest.mark.skip
 def test_scale():
