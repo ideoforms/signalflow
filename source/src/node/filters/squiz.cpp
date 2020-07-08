d54393923b4f64516de82dc477e3202b0a57ff22
Daniel Jones <daniel@jones.org.uk>
2020-07-08 22:40:48 +0100

    Improved Squix

--- source/src/node/filters/squiz.cpp
+++ source/src/node/filters/squiz.cpp
@@ -3,7 +3,7 @@
 namespace libsignal
 {
 
-int LOOKAHEAD_FRAMES = 44100;
+int SIGNAL_SQUIZ_LOOKAHEAD_FRAMES = 2205;
 
 Squiz::Squiz(NodeRef input, NodeRef rate, NodeRef chunk_size)
     : UnaryOpNode(input), rate(rate), chunk_size(chunk_size)
@@ -14,12 +14,13 @@ Squiz::Squiz(NodeRef input, NodeRef rate, NodeRef chunk_size)
 
     for (int i = 0; i < SIGNAL_MAX_CHANNELS; i++)
     {
-        BufferRef buf = new Buffer(1, LOOKAHEAD_FRAMES);
+        BufferRef buf = new Buffer(1, SIGNAL_SQUIZ_LOOKAHEAD_FRAMES);
         this->buffers.push_back(buf);
-        this->write_pos.push_back(LOOKAHEAD_FRAMES / 2);
+        this->write_pos.push_back(SIGNAL_SQUIZ_LOOKAHEAD_FRAMES / 2);
         this->read_pos.push_back(0.0f);
-        this->read_pos_accelerated.push_back(0.0f);
-        this->chunk_counter.push_back(0);
+        this->read_pos_chunk_counter.push_back(0);
+        this->repitched_read_pos.push_back(0.0f);
+        this->repitched_read_pos_chunk_counter.push_back(0);
     }
 }
 
@@ -29,21 +30,25 @@ void Squiz::process(sample **out, int num_frames)
     {
         for (int frame = 0; frame < num_frames; frame++)
         {
-            if (this->chunk_counter[channel] < this->chunk_size->out[channel][frame])
+            if (this->repitched_read_pos_chunk_counter[channel] < this->chunk_size->out[channel][frame])
             {
-                sample read_last = this->buffers[channel]->get(this->read_pos_accelerated[channel]);
-                this->read_pos_accelerated[channel] += this->rate->out[channel][frame];
-                if (this->read_pos_accelerated[channel] >= LOOKAHEAD_FRAMES)
-                    this->read_pos_accelerated[channel] -= LOOKAHEAD_FRAMES;
-                sample read_cur = this->buffers[channel]->get(this->read_pos_accelerated[channel]);
+                sample read_last = this->buffers[channel]->get(this->repitched_read_pos[channel]);
+                this->repitched_read_pos[channel] += this->rate->out[channel][frame];
+                if (this->repitched_read_pos[channel] >= SIGNAL_SQUIZ_LOOKAHEAD_FRAMES)
+                    this->repitched_read_pos[channel] -= SIGNAL_SQUIZ_LOOKAHEAD_FRAMES;
+                sample read_cur = this->buffers[channel]->get(this->repitched_read_pos[channel]);
                 if (read_cur > 0 && read_last <= 0)
                 {
-                    this->chunk_counter[channel]++;
-                    this->out[channel][frame] = 0.0;
+                    this->repitched_read_pos_chunk_counter[channel]++;
+                }
+
+                if (this->repitched_read_pos_chunk_counter[channel] < this->chunk_size->out[channel][frame])
+                {
+                    this->out[channel][frame] = read_cur;
                 }
                 else
                 {
-                    this->out[channel][frame] = read_cur;
+                    this->out[channel][frame] = 0.0;
                 }
             }
             else
@@ -53,21 +58,28 @@ void Squiz::process(sample **out, int num_frames)
 
             sample read_last = this->buffers[channel]->get(this->read_pos[channel]);
             this->read_pos[channel] += 1;
-            if (this->read_pos[channel] >= LOOKAHEAD_FRAMES)
-                this->read_pos[channel] -= LOOKAHEAD_FRAMES;
+            if (this->read_pos[channel] >= SIGNAL_SQUIZ_LOOKAHEAD_FRAMES)
+                this->read_pos[channel] -= SIGNAL_SQUIZ_LOOKAHEAD_FRAMES;
             sample read_cur = this->buffers[channel]->get(this->read_pos[channel]);
 
             if (read_cur > 0 && read_last <= 0)
             {
-                this->chunk_counter[channel] = false;
-                this->read_pos_accelerated[channel] = this->read_pos[channel];
+                this->read_pos_chunk_counter[channel] += 1;
+
+                // if (this->read_pos_chunk_counter[channel] >= this->chunk_size->out[channel][frame])
+                if (this->read_pos_chunk_counter[channel] >= 1)
+                {
+                    this->read_pos_chunk_counter[channel] = 0;
+                    this->repitched_read_pos_chunk_counter[channel] = 0;
+                    this->repitched_read_pos[channel] = this->read_pos[channel];
+                }
             }
 
             this->buffers[channel]->data[0][this->write_pos[channel]] = this->input->out[channel][frame];
 
             this->write_pos[channel] += 1;
-            if (this->write_pos[channel] >= LOOKAHEAD_FRAMES)
-                this->write_pos[channel] -= LOOKAHEAD_FRAMES;
+            if (this->write_pos[channel] >= SIGNAL_SQUIZ_LOOKAHEAD_FRAMES)
+                this->write_pos[channel] -= SIGNAL_SQUIZ_LOOKAHEAD_FRAMES;
         }
     }
 }
