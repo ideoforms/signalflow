5c3266de52d13f9a81272939d32ea02f64979e00
Daniel Jones <daniel@jones.org.uk>
2020-07-10 00:29:55 +0100

    Expand test_patch to test node inputs and buffer inputs

--- tests/test_patch.py
+++ tests/test_patch.py
@@ -1,19 +1,31 @@
-from libsignal import PatchSpec, Patch
-from libsignal import Sine, EnvelopeASR
+from libsignal import PatchSpec, Patch, Buffer, BufferPlayer
+from libsignal import Multiply, Sine, EnvelopeASR
 from . import graph
+import numpy as np
 
 def test_patch(graph):
     prototype = Patch()
-    sine = prototype.add_node(Sine(440))
-    prototype.set_output(sine)
+    buf_in = prototype.add_buffer_input("buffer")
+    input = prototype.add_input("input")
+    player = prototype.add_node(BufferPlayer(buf_in, loop=True))
+    output = prototype.add_node(Multiply(input, player))
+    prototype.set_output(output)
     spec = prototype.create_spec()
 
     patch = Patch(spec)
-    assert len(patch.nodes) == 2
-    assert len(patch.inputs) == 0
-    assert patch.output.name == "sine"
+    buf_in = Buffer([ 2, 2, 2, 2 ])
+    patch.set_input("input", 123)
+    patch.set_input("buffer", buf_in)
+    assert len(patch.nodes) == 5
+    assert len(patch.inputs) == 1
+    assert patch.output.name == "multiply"
     assert patch.output.patch == patch
     assert patch.auto_free == False
+
+    graph.add_output(patch)
+    b = Buffer(1, 1000)
+    graph.render_to_buffer(b)
+    assert np.all(b.data[0] == 246)
 
     patch.auto_free = True
     assert patch.auto_free == True
