2bf521bb69750a6961a01ecfcc777ccbc4a4a232
Daniel Jones <daniel@jones.org.uk>
2020-07-24 00:22:31 +0100

    Add unit test to construct all Node subclasses

--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -9,9 +9,12 @@ from . import graph
 def test_nodes_general():
     """
     Try instantiating each Node class in turn, check that no crashes take place.
+    Also checks that names and registration IDs are identical.
     """
 
     names = []
+    registry = signalflow.NodeRegistry()
+
     for key, value in vars(signalflow).items():
         if inspect.isclass(value) and issubclass(value, signalflow.Node):
             if key != "Node":
@@ -29,3 +32,5 @@ def test_nodes_general():
                     #--------------------------------------------------------------------------------
                     assert re.match("^[a-z0-9-]+$", name)
                     names.append(name)
+
+                    node = registry.create(name)
