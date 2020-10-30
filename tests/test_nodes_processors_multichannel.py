c8e7f799cd10ef4bf86d05c8a50b0640f4c920fa
Daniel Jones <daniel@jones.org.uk>
2020-10-30 01:19:03 +0000

    Add test_nodes_processors_multichannel.py

---
+++ tests/test_nodes_processors_multichannel.py
@@ -0,0 +1,17 @@
+import signalflow as sf
+from . import graph
+
+import pytest
+
+def test_nodes_multichannel_pan(graph):
+    a = 0.5
+    b = sf.LinearPanner(2, a, 0.0)
+    graph.render_subgraph(b)
+    assert all(b.output_buffer[0] == 0.5)
+    assert all(b.output_buffer[1] == 0.0)
+
+    b.set_input("pan", 0.5)
+    graph.reset_subgraph(b)
+    graph.render_subgraph(b)
+    assert all(b.output_buffer[0] == 0.25)
+    assert all(b.output_buffer[1] == 0.25)
