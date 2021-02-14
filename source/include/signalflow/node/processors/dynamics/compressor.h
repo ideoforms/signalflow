7dee147616aed0073830327e55b262f6ac9ddf5b
Daniel Jones <daniel@jones.org.uk>
2021-02-14 12:57:35 +0000

    More reasonable Compressor defaults

--- source/include/signalflow/node/processors/dynamics/compressor.h
+++ source/include/signalflow/node/processors/dynamics/compressor.h
@@ -9,10 +9,10 @@ class Compressor : public UnaryOpNode
 {
 public:
     Compressor(NodeRef input = 0.0,
-               NodeRef threshold = 0.5,
-               NodeRef ratio = 1,
-               NodeRef attack_time = 0.001,
-               NodeRef release_time = 0.01,
+               NodeRef threshold = 0.1,
+               NodeRef ratio = 2,
+               NodeRef attack_time = 0.01,
+               NodeRef release_time = 0.1,
                NodeRef sidechain = nullptr);
 
     virtual void process(Buffer &out, int num_frames);
