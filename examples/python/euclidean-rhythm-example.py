4b77500b906b6d763be5ad6e208dc3ce4f3d40cc
Daniel Jones <daniel@jones.org.uk>
2021-02-27 13:16:07 +0000

    Add Sum/Tanh to Euclidean example

--- examples/python/euclidean-rhythm-example.py
+++ examples/python/euclidean-rhythm-example.py
@@ -11,22 +11,26 @@ import random
 graph = AudioGraph(start=True)
 
 clock = Impulse(10)
+summer = Sum()
 
 eu0 = Euclidean(ClockDivider(clock, 2), 24, 7)
 flt0 = SVFFilter(eu0, "band_pass", cutoff=80, resonance=0.99) * 9
-graph.play(LinearPanner(2, flt0, 0.0))
+summer.add_input(LinearPanner(2, flt0, 0.0))
 
 eu1 = Euclidean(clock, 13, 9)
 flt1 = SVFFilter(eu1, "band_pass", cutoff=800, resonance=0.98) * 0.2
-graph.play(LinearPanner(2, flt1, -0.5))
+summer.add_input(LinearPanner(2, flt1, -0.5))
 
 eu2 = Euclidean(clock, 16, 11)
 flt2 = SVFFilter(eu2, "band_pass", cutoff=8000, resonance=0.97) * 0.1
-graph.play(LinearPanner(2, flt2, 0.5))
+summer.add_input(LinearPanner(2, flt2, 0.5))
 
 eu3 = Euclidean(ClockDivider(clock, 2), 19, 12)
 reson = RandomExponential(0.99, 0.999, eu3)
 flt3 = SVFFilter(eu3, "band_pass", cutoff=480, resonance=reson) * 0.2
-graph.play(LinearPanner(2, flt3, 0))
+summer.add_input(LinearPanner(2, flt3, 0))
+
+tanh = Tanh(summer * 20)
+tanh.play()
 
 graph.wait()
