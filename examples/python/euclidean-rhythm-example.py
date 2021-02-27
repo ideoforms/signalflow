71cc5bec1bfff58e90c0568ea6a5bf935e57650f
Daniel Jones <daniel@jones.org.uk>
2021-02-27 22:38:45 +0000

    Update Euclidean example to use Patch

--- examples/python/euclidean-rhythm-example.py
+++ examples/python/euclidean-rhythm-example.py
@@ -10,27 +10,26 @@ import random
 
 graph = AudioGraph(start=True)
 
-clock = Impulse(10)
-summer = Sum()
+patch = Patch()
+divider = patch.add_input("divider", 1.0)
+amp = patch.add_input("amp", 1.0)
+clock = patch.add_input("clock", 0)
+length = patch.add_input("length", 24)
+events = patch.add_input("events", 8)
+cutoff = patch.add_input("frequency", 8000)
+resonance = patch.add_input("resonance", 0.97)
+pan = patch.add_input("pan", 0.0)
+clock = ClockDivider(clock, divider)
+eu = patch.add_node(Euclidean(clock, length, events))
+flt = SVFFilter(eu, "band_pass", cutoff=cutoff, resonance=resonance)
+panned = LinearPanner(2, flt * amp, pan)
+patch.set_output(panned)
+spec = patch.create_spec()
 
-eu0 = Euclidean(ClockDivider(clock, 2), 24, 7)
-flt0 = SVFFilter(eu0, "band_pass", cutoff=80, resonance=0.99) * 9
-summer.add_input(LinearPanner(2, flt0, 0.0))
-
-eu1 = Euclidean(clock, 13, 9)
-flt1 = SVFFilter(eu1, "band_pass", cutoff=800, resonance=0.98) * 0.2
-summer.add_input(LinearPanner(2, flt1, -0.5))
-
-eu2 = Euclidean(clock, 16, 11)
-flt2 = SVFFilter(eu2, "band_pass", cutoff=8000, resonance=0.97) * 0.1
-summer.add_input(LinearPanner(2, flt2, 0.5))
-
-eu3 = Euclidean(ClockDivider(clock, 2), 19, 12)
-reson = RandomExponential(0.99, 0.999, eu3)
-flt3 = SVFFilter(eu3, "band_pass", cutoff=480, resonance=reson) * 0.2
-summer.add_input(LinearPanner(2, flt3, 0))
-
-tanh = Tanh(summer * 20)
-tanh.play()
+clock = Impulse(8)
+Patch(spec, { "clock" : clock, "divider": 2, "length": 24,  "events": 7, "frequency": 80, "resonance": 0.99, "pan": 0.0, "amp": 20 }).play()
+Patch(spec, { "clock" : clock, "divider": 1, "length": 13, "events": 9, "frequency": 800, "resonance": 0.98, "pan": 0.7, "amp": 0.2 }).play()
+Patch(spec, { "clock" : clock, "divider": 1, "length": 16, "events": 11, "frequency": 8000, "resonance": 0.97, "pan": -0.7, "amp": 0.1 }).play()
+Patch(spec, { "clock" : clock, "divider": 2, "length": 19, "events": 12, "frequency": 480, "resonance": 0.99, "amp": 0.2 }).play()
 
 graph.wait()
