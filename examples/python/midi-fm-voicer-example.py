293b1decce8363484c1e3c8f2788bf4ad55fc87d
Daniel Jones <daniel@jones.org.uk>
2021-03-13 11:45:57 +0000

    Add MIDI gate support to FM voicer example

--- examples/python/midi-fm-voicer-example.py
+++ examples/python/midi-fm-voicer-example.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #------------------------------------------------------------------------
-# SignalFlow: FM synthesis with MIDI keyboard trigger.
+# SignalFlow: FM synthesis, controlled by a MIDI input device.
 # The mido module is required for this example:
 #
 #   pip3 install mido python-rtmidi
@@ -20,9 +20,9 @@ import mido
 #   envelope is an ASR envelope with attack, sustain, release in seconds
 #--------------------------------------------------------------------------------
 params = [
-    [ 1.0, 1.0, 0, [ 0.0, 0.3, 0.5 ] ],
-    [ 2.0, 0.75, 0, [ 0.0, 0.1, 0.5 ] ],
-    [ 3.5, 0.6, 0, [ 0.0, 0.1, 0.5 ] ]
+    [ 1.0, 1.0, 0, [ 0.0, 0.3, 0.7, 0.9 ] ],
+    [ 2.0, 0.75, 0, [ 0.0, 0.4, 0.7, 0.7 ] ],
+    [ 3.17, 0.9, 0, [ 0.0, 0.1, 0.7, 0.8 ] ]
 ]
 
 #------------------------------------------------------------------------
@@ -32,47 +32,48 @@ graph = AudioGraph(start=True)
 graph.show_status(1)
 
 class FM3 (Patch):
-    def __init__(self, f0, params):
+    def __init__(self, frequency, params):
         super().__init__()
-        op2 = FMOp(f0, *params[2])
-        op1 = FMOp(f0, *params[1], fm=op2)
-        op0 = FMOp(f0, *params[0], fm=op1)
+        gate = self.add_input("gate", 1)
+        op2 = FMOp(frequency, *params[2], gate)
+        op1 = FMOp(frequency, *params[1], gate, fm=op2)
+        op0 = FMOp(frequency, *params[0], gate, fm=op1+op2)
         stereo = LinearPanner(2, op0 + op1)
         self.set_output(stereo * 0.1)
 
 class FMOp (Patch):
-    def __init__(self, f0, coarse, level, detune, env, fm=None):
+    def __init__(self, f0, coarse, level, detune, env, gate, fm=None):
         """
         FM operator that follows the exponential output curve of the Yamaha DX7:
         https://sound.stackexchange.com/a/42979/33063
         """
         super().__init__()
         amplitude = ScaleLinExp(level, 0, 1, 0.0005, 1)
-        freq = f0 * coarse
+        frequency = f0 * coarse
         if fm is not None:
-            freq = freq + (f0 * fm * 14)
-        freq = freq * (1 + 0.0005 * detune)
-        sine = SineOscillator(freq)
-        env = EnvelopeASR(env[0], env[1], env[2], curve=2)
+            frequency = frequency + (f0 * fm * 14)
+        frequency = frequency * (1 + 0.0005 * detune)
+        sine = SineOscillator(frequency)
+        env = EnvelopeADSR(*env, gate=gate)
         self.set_output(sine * env * amplitude)
 
 
 class MIDIVoicer:
-    def __init__(self, device_name=None, voice_count=16):
+    def __init__(self, device_name=None):
         self.input = mido.open_input(device_name)
-        self.voice_count = voice_count
-        self.voices = [None] * voice_count
-        self.voice_index = 0
+        self.notes = [None] * 128
 
     def run(self):
         for message in self.input:
             if message.type == 'note_on':
                 voice = FM3(midi_note_to_frequency(message.note), params)
                 voice.play()
-                if self.voices[self.voice_index]:
-                    self.voices[self.voice_index].stop()
-                self.voices[self.voice_index] = voice
-                self.voice_index = (self.voice_index + 1) % self.voice_count
+                voice.auto_free = True
+                self.notes[message.note] = voice
+            elif message.type == 'note_off':
+                if self.notes[message.note]:
+                    self.notes[message.note].set_input("gate", 0)
+                    self.notes[message.note] = None
 
 voicer = MIDIVoicer()
 voicer.run()
