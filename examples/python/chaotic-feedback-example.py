4161095f0c857f9a0b764954a6d8d79d923ce198
Daniel Jones <daniel@jones.org.uk>
2021-03-19 18:49:50 +0000

    Slow down clock in chaotic-feedback-example

--- examples/python/chaotic-feedback-example.py
+++ examples/python/chaotic-feedback-example.py
@@ -30,7 +30,7 @@ op0 = SineOscillator(f0 + f0 * feedback * 14)
 # Pass an array to RandomImpulse to generate different randomised 
 # sequences in the L/R channels, for stereo output.
 #------------------------------------------------------------------------
-level = Smooth(RandomUniform(0, 1, clock=RandomImpulse([1, 1])))
+level = Smooth(RandomUniform(0, 1, clock=RandomImpulse([0.5, 0.5])))
 level = ScaleLinExp(level, 0, 1, 0.0001, 1.0)
 op0 = op0 * level
 graph.add_node(FeedbackBufferWriter(buf, op0, 0.5))
