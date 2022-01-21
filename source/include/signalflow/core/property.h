285d7ef2c095da15611e0c5721695a3e10194646
Daniel Jones <daniel@jones.org.uk>
2022-01-21 18:18:59 +0000

    Add signalflow_property_type_t to query property type

--- source/include/signalflow/core/property.h
+++ source/include/signalflow/core/property.h
@@ -8,10 +8,22 @@
 namespace signalflow
 {
 
+typedef enum : unsigned int
+{
+    SIGNALFLOW_PROPERTY_TYPE_NONE,
+    SIGNALFLOW_PROPERTY_TYPE_INT,
+    SIGNALFLOW_PROPERTY_TYPE_FLOAT,
+    SIGNALFLOW_PROPERTY_TYPE_FLOAT_ARRAY,
+    SIGNALFLOW_PROPERTY_TYPE_STRING,
+    SIGNALFLOW_PROPERTY_TYPE_STRING_ARRAY
+} signalflow_property_type_t;
+
 class Property
 {
 public:
     virtual ~Property() {}
+
+    virtual signalflow_property_type_t get_property_type() { return SIGNALFLOW_PROPERTY_TYPE_NONE; }
 
     virtual int int_value() { return 0; }
     virtual float float_value() { return 0.0; }
@@ -43,8 +55,11 @@ class FloatProperty : public TypedProperty<float>
 public:
     FloatProperty(float value)
         : TypedProperty(value) {}
+
+    virtual signalflow_property_type_t get_property_type() override { return SIGNALFLOW_PROPERTY_TYPE_FLOAT; }
     virtual int int_value() override { return value; }
     virtual float float_value() override { return value; }
+    virtual std::string string_value() override { return std::to_string(value); }
     virtual void set_int_value(int new_value) override { value = new_value; }
     virtual void set_float_value(float new_value) override { value = new_value; }
 };
@@ -54,8 +69,11 @@ class IntProperty : public TypedProperty<int>
 public:
     IntProperty(int value)
         : TypedProperty(value) {}
+
+    virtual signalflow_property_type_t get_property_type() override { return SIGNALFLOW_PROPERTY_TYPE_INT; }
     virtual int int_value() override { return value; }
     virtual float float_value() override { return value; }
+    virtual std::string string_value() override { return std::to_string(value); }
     virtual void set_int_value(int new_value) override { value = new_value; }
     virtual void set_float_value(float new_value) override { value = new_value; }
 };
@@ -65,6 +83,8 @@ class StringProperty : public TypedProperty<std::string>
 public:
     StringProperty(std::string value)
         : TypedProperty(value) {}
+
+    virtual signalflow_property_type_t get_property_type() override { return SIGNALFLOW_PROPERTY_TYPE_STRING; }
     virtual std::string string_value() override { return value; }
     virtual void set_string_value(std::string new_value) override { value = new_value; }
 };
@@ -74,7 +94,10 @@ class FloatArrayProperty : public TypedProperty<std::vector<float>>
 public:
     FloatArrayProperty(std::vector<float> value)
         : TypedProperty(value) {}
+
+    virtual signalflow_property_type_t get_property_type() override { return SIGNALFLOW_PROPERTY_TYPE_FLOAT_ARRAY; }
     virtual std::vector<float> float_array_value() override { return value; }
+    // TODO: virtual std::string string_value() override {  }
     virtual void set_float_array_value(std::vector<float> new_value) override { value = new_value; }
 };
 
@@ -83,7 +106,10 @@ class StringArrayProperty : public TypedProperty<std::vector<std::string>>
 public:
     StringArrayProperty(std::vector<std::string> value)
         : TypedProperty(value) {}
+
+    virtual signalflow_property_type_t get_property_type() override { return SIGNALFLOW_PROPERTY_TYPE_STRING_ARRAY; }
     virtual std::vector<std::string> string_array_value() override { return value; }
+    // TODO: virtual std::string string_value() override {  }
     virtual void set_string_array_value(std::vector<std::string> new_value) override { value = new_value; }
 };
 
