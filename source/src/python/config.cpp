725f251e55473f2320a49719d976ffb8fa9c1e3c
Daniel Jones <daniel@jones.org.uk>
2022-04-01 14:38:19 +0100

    Expose AudioGraphConfig::cpu_usage_limit

--- source/src/python/config.cpp
+++ source/src/python/config.cpp
@@ -11,5 +11,6 @@ void init_python_config(py::module &m)
         .def_property("input_buffer_size", &AudioGraphConfig::get_input_buffer_size, &AudioGraphConfig::set_input_buffer_size)
         .def_property("output_buffer_size", &AudioGraphConfig::get_output_buffer_size, &AudioGraphConfig::set_output_buffer_size)
         .def_property("input_device_name", &AudioGraphConfig::get_input_device_name, &AudioGraphConfig::set_input_device_name)
-        .def_property("output_device_name", &AudioGraphConfig::get_output_device_name, &AudioGraphConfig::set_output_device_name);
+        .def_property("output_device_name", &AudioGraphConfig::get_output_device_name, &AudioGraphConfig::set_output_device_name)
+        .def_property("cpu_usage_limit", &AudioGraphConfig::get_cpu_usage_limit, &AudioGraphConfig::set_cpu_usage_limit);
 }
