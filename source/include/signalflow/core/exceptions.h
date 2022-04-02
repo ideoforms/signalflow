2ea45ba96295a8e5c952bac2cf1c8e853dcbc4d2
Daniel Jones <daniel@jones.org.uk>
2022-04-02 14:50:02 +0100

    Add patch_finished_playback_exception

--- source/include/signalflow/core/exceptions.h
+++ source/include/signalflow/core/exceptions.h
@@ -35,4 +35,14 @@ struct invalid_channel_count_exception : public std::runtime_error
         : std::runtime_error(message) {}
 };
 
+struct patch_finished_playback_exception : public std::runtime_error
+{
+    using std::runtime_error::runtime_error;
+
+    patch_finished_playback_exception()
+        : std::runtime_error("Cannot play Patch as it has already finished playback") {}
+    patch_finished_playback_exception(const char *message)
+        : std::runtime_error(message) {}
+};
+
 }
