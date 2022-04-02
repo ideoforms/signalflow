b04b26cda3c26fbcfcd48e458997c9c81625672e
Daniel Jones <daniel@jones.org.uk>
2022-04-02 14:52:25 +0100

    Add PatchFinishedPlaybackException

--- source/src/python/exceptions.cpp
+++ source/src/python/exceptions.cpp
@@ -8,4 +8,5 @@ void init_python_exceptions(py::module &m)
     py::register_exception<signalflow::graph_not_created_exception>(m, "GraphNotCreatedException");
     py::register_exception<signalflow::graph_already_created_exception>(m, "GraphAlreadyCreatedException");
     py::register_exception<signalflow::invalid_channel_count_exception>(m, "InvalidChannelCountException");
+    py::register_exception<signalflow::patch_finished_playback_exception>(m, "PatchFinishedPlaybackException");
 }
