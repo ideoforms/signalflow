2c9a473c6c9fc8affbe94b31e5d2f0f8c3adad86
Daniel Jones <daniel@jones.org.uk>
2023-05-27 17:49:22 -0600

    Add make-macos-x86-arm64.sh build script

---
+++ auxiliary/scripts/cibuildwheel/make-macos-x86-arm64.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#--------------------------------------------------------------------------------
+# Builds arm64 and x86_64 wheels for macOS
+# Requires virtualenvs in the current directory, named venv-38, venv-39, etc...
+# Should be retired when GitHub Actions support arm64 build
+#--------------------------------------------------------------------------------
+
+for VERSION in 38 39 310 311
+do
+    rm -r build
+    export CIBW_BUILD="cp${VERSION}-*"
+    export CIBW_BUILD_VERBOSITY=2
+
+    . venv-$VERSION/bin/activate
+    pip3 install cibuildwheel
+
+    #--------------------------------------------------------------------------------
+    # Make x86
+    #--------------------------------------------------------------------------------
+    export REPAIR_LIBRARY_PATH=/usr/local/lib
+    export CIBW_ARCHS_MACOS="x86_64"
+    export CMAKE_OSX_ARCHITECTURES=x86_64
+    export CIBW_REPAIR_WHEEL_COMMAND_MACOS="DYLD_LIBRARY_PATH=$REPAIR_LIBRARY_PATH delocate-wheel --require-archs {delocate_archs} -w {dest_dir} -v {wheel}"
+
+    python3 -m cibuildwheel --output-dir wheelhouse --platform macos
+
+    #--------------------------------------------------------------------------------
+    # Make arm64
+    #--------------------------------------------------------------------------------
+    export REPAIR_LIBRARY_PATH=/opt/homebrew/lib
+    export CIBW_ARCHS_MACOS="arm64"
+    export CMAKE_OSX_ARCHITECTURES=arm64
+    export CIBW_REPAIR_WHEEL_COMMAND_MACOS="DYLD_LIBRARY_PATH=$REPAIR_LIBRARY_PATH delocate-wheel --require-archs {delocate_archs} -w {dest_dir} -v {wheel}"
+
+    python3 -m cibuildwheel --output-dir wheelhouse --platform macos
+done
