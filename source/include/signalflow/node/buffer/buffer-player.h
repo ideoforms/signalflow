1200e390619ec683a87c70fb64a5e69b7fd1205a
Daniel Jones <daniel@jones.org.uk>
2023-07-29 23:51:34 +0100

    Fix warnings in buffer-player.h

--- source/include/signalflow/node/buffer/buffer-player.h
+++ source/include/signalflow/node/buffer/buffer-player.h
@@ -10,9 +10,9 @@ public:
     BufferPlayer(BufferRef buffer = nullptr, NodeRef rate = 1.0, NodeRef loop = 0,
                  NodeRef start_time = nullptr, NodeRef end_time = nullptr, NodeRef clock = nullptr);
 
-    virtual void set_buffer(std::string, BufferRef buffer);
-    virtual void trigger(std::string = SIGNALFLOW_DEFAULT_TRIGGER, float value = 0.0);
-    virtual void process(Buffer &out, int num_frames);
+    virtual void set_buffer(std::string, BufferRef buffer) override;
+    virtual void trigger(std::string = SIGNALFLOW_DEFAULT_TRIGGER, float value = 0.0) override;
+    virtual void process(Buffer &out, int num_frames) override;
     virtual PropertyRef get_property(std::string name) override;
 
     BufferRef buffer;
