fef0014a4737d26586418e4dfc9c9cbd58bed3a1
Gregory White <gcwhitemail@gmail.com>
2023-12-05 16:56:19 +0000

    Documentation: Updating SawLFO
    Updating index.md and adding example-0.py

---
+++ docs/library/oscillators/sawlfo/example-0.py
@@ -0,0 +1,8 @@
+from signalflow import *
+graph = AudioGraph()
+
+# Siren effect, using a sawtooth LFO to modulate a sinewave's frequency
+lfo = SawLFO(1, 200, 1000)
+sine = SineOscillator(lfo)
+sine.play()
+graph.wait()
