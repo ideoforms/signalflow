597977056e46c0bc5188e5ff8050105e0aabdcfa
Daniel Jones <daniel@jones.org.uk>
2024-01-11 12:03:24 +0000

    Patch: Export node IDs to json sequentially

--- tests/test_patch.py
+++ tests/test_patch.py
@@ -101,7 +101,7 @@ def test_patch_to_json(graph):
     patch = TestPatch()
     spec = patch.to_spec()
     spec_json = spec.to_json()
-    ref_json = '{"buffer_inputs": [], "inputs": [], "name": "", "nodes": [{"id": 4, "inputs": {"attack": 0.1, "curve": 1, "release": 0.1, "sustain": 0.5}, "node": "asr-envelope"}, {"id": 2, "inputs": {"frequency": 440}, "node": "sine"}, {"id": 1, "inputs": {"input0": {"id": 2}, "input1": {"id": 4}}, "node": "multiply"}, {"id": 0, "inputs": {"input": {"id": 1}, "pan": 0}, "is_output": true, "node": "stereo-panner"}]}'
+    ref_json = '{"buffer_inputs": [], "inputs": [], "name": "", "nodes": [{"id": 0, "inputs": {"input": {"id": 1}, "pan": 0}, "is_output": true, "node": "stereo-panner"}, {"id": 1, "inputs": {"input0": {"id": 2}, "input1": {"id": 4}}, "node": "multiply"}, {"id": 2, "inputs": {"frequency": 440}, "node": "sine"}, {"id": 4, "inputs": {"attack": 0.1, "curve": 1, "release": 0.1, "sustain": 0.5}, "node": "asr-envelope"}]}'
 
     #--------------------------------------------------------------------------------
     # Can't simply compare the to_json output to the ref_json, as ordering of objects
