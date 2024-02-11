3b93edc64aeeebd6956c3841dd8b31ddf9266075
Daniel Jones <daniel@jones.org.uk>
2024-02-11 21:54:32 +0000

    FFTContrast: Tweak algorithm

--- source/src/node/fft/fft-contrast.cpp
+++ source/src/node/fft/fft-contrast.cpp
@@ -18,10 +18,12 @@ void FFTContrast::process(Buffer &out, int num_frames)
 
     for (int hop = 0; hop < this->num_hops; hop++)
     {
-        //        memcpy(this->mags, &input->out[hop][0], sizeof(float) * num_bins);
-        //        std::sort(this->mags, mags + num_bins);
-        //        float average_magnitude = this->mags[(int) (this->num_bins / 2)];
-        float average_magnitude = signalflow_array_mean(&this->out[hop][0], num_bins);
+        memcpy(this->mags, &input->out[hop][0], sizeof(float) * num_bins);
+        std::sort(this->mags, mags + num_bins);
+        float average_magnitude = this->mags[(int) (this->num_bins / 2)];
+        float min_magnitude = this->mags[0];
+        float max_magnitude = this->mags[this->num_bins - 1];
+        //        float average_magnitude = signalflow_array_mean(&this->out[hop][0], num_bins);
 
         for (int frame = 0; frame < this->fft_size; frame++)
         {
@@ -31,7 +33,7 @@ void FFTContrast::process(Buffer &out, int num_frames)
                 float input_value = this->input->out[hop][frame];
                 float input_delta_from_average = input_value - average_magnitude;
                 float output_value = average_magnitude + (contrast * input_delta_from_average);
-                output_value = signalflow_clip(output_value, 0, this->num_bins);
+                output_value = signalflow_clip(output_value, min_magnitude, max_magnitude);
 
                 out[hop][frame] = output_value;
             }
