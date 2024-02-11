aebf612eb32d34c8d3fcaaa8c1ef462fde14eca2
Daniel Jones <daniel@jones.org.uk>
2024-02-11 21:53:19 +0000

    FFTFlipSpectrum: Fix and comment

--- source/src/node/fft/fft-flip-spectrum.cpp
+++ source/src/node/fft/fft-flip-spectrum.cpp
@@ -19,18 +19,22 @@ void FFTFlipSpectrum::process(Buffer &out, int num_frames)
 
     for (int hop = 0; hop < this->num_hops; hop++)
     {
+        // One of the gotchas of FFT nodes is that they can't be modulated at audio
+        // rate as they process at a different rate (in FFT-size'd blocks of samples)
+        // so just take the first value of the flip/rotate inputs.
+        bool flip = this->flip->out[0][0];
+        int rotate = (int) this->rotate->out[0][0];
+
         for (int frame = 0; frame < this->fft_size; frame++)
         {
             if (frame < this->num_bins)
             {
-                bool flip = this->flip->out[0][frame];
-                int rotate = (int) this->rotate->out[0][frame];
-                int frame_after_transform = frame;
+                int frame_after_transform = frame - rotate;
                 if (flip)
                 {
                     frame_after_transform = this->num_bins - 1 - frame_after_transform;
                 }
-                frame_after_transform = (frame_after_transform - rotate) % this->num_bins;
+                frame_after_transform = frame_after_transform % this->num_bins;
                 if (frame_after_transform < 0)
                 {
                     frame_after_transform += this->num_bins;
