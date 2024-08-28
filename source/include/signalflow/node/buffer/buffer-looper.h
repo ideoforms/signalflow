733294ffa1c8eca686f0ba2cf25c1eafe5cedf19
Daniel Jones <daniel@jones.org.uk>
2024-08-28 14:56:16 +0100

    BufferLooper: Separate looping and playback heads; add looper_level and playthrough_level

--- source/include/signalflow/node/buffer/buffer-looper.h
+++ source/include/signalflow/node/buffer/buffer-looper.h
@@ -14,21 +14,30 @@ public:
     BufferLooper(BufferRef buffer = nullptr,
                  NodeRef input = 0.0,
                  NodeRef feedback = 0.0,
-                 bool loop_playback = false,
-                 bool loop_record = false);
+                 NodeRef loop_playback = 0,
+                 NodeRef loop_record = 0,
+                 NodeRef start_time = nullptr,
+                 NodeRef end_time = nullptr,
+                 NodeRef looper_level = 1.0,
+                 NodeRef playthrough_level = 0.0);
 
     virtual void process(Buffer &out, int num_frames);
     virtual void trigger(std::string = SIGNALFLOW_DEFAULT_TRIGGER, float value = 0.0);
 
     BufferRef buffer;
 
+private:
+    float playback_phase;
+    float record_phase;
+
     NodeRef input;
     NodeRef feedback;
-
-private:
-    float phase;
-    bool loop_playback;
-    bool loop_record;
+    NodeRef loop_playback;
+    NodeRef loop_record;
+    NodeRef start_time;
+    NodeRef end_time;
+    NodeRef looper_level;
+    NodeRef playthrough_level;
 
     bool is_recording;
     bool is_playing;
--- source/src/node/buffer/buffer-looper.cpp
+++ source/src/node/buffer/buffer-looper.cpp
@@ -1,11 +1,15 @@
+#include "signalflow/core/core.h"
+#include "signalflow/core/graph.h"
 #include "signalflow/node/buffer/buffer-looper.h"
 
 namespace signalflow
 {
 
 BufferLooper::BufferLooper(BufferRef buffer, NodeRef input, NodeRef feedback,
-                           bool loop_playback, bool loop_record)
-    : buffer(buffer), input(input), feedback(feedback), loop_playback(loop_playback), loop_record(loop_record)
+                           NodeRef loop_playback, NodeRef loop_record,
+                           NodeRef start_time, NodeRef end_time,
+                           NodeRef looper_level, NodeRef playthrough_level)
+    : buffer(buffer), input(input), feedback(feedback), loop_playback(loop_playback), loop_record(loop_record), start_time(start_time), end_time(end_time), looper_level(looper_level), playthrough_level(playthrough_level)
 {
     SIGNALFLOW_CHECK_GRAPH()
 
@@ -14,6 +18,12 @@ BufferLooper::BufferLooper(BufferRef buffer, NodeRef input, NodeRef feedback,
     this->create_buffer("buffer", this->buffer);
     this->create_input("input", this->input);
     this->create_input("feedback", this->feedback);
+    this->create_input("loop_playback", this->loop_playback);
+    this->create_input("loop_record", this->loop_record);
+    this->create_input("start_time", this->start_time);
+    this->create_input("end_time", this->end_time);
+    this->create_input("looper_level", this->looper_level);
+    this->create_input("playthrough_level", this->playthrough_level);
 
     if (buffer)
     {
@@ -24,7 +34,8 @@ BufferLooper::BufferLooper(BufferRef buffer, NodeRef input, NodeRef feedback,
         this->set_buffer("buffer", buffer);
     }
 
-    this->phase = 0.0;
+    this->playback_phase = 0.0;
+    this->record_phase = 0.0;
     this->is_playing = false;
     this->is_recording = false;
 }
@@ -33,29 +44,29 @@ void BufferLooper::trigger(std::string name, float value)
 {
     if (name == SIGNALFLOW_DEFAULT_TRIGGER)
     {
-        this->phase = 0.0;
+        if (start_time)
+            this->playback_phase = (int) this->start_time->out[0][0] * this->graph->get_sample_rate();
+        else
+            this->playback_phase = 0;
     }
     else if (name == SIGNALFLOW_TRIGGER_PLAY)
     {
-        if (this->state == SIGNALFLOW_NODE_STATE_STOPPED)
-        {
-            this->phase = 0.0;
-        }
+        if (start_time)
+            this->playback_phase = (int) this->start_time->out[0][0] * this->graph->get_sample_rate();
+        else
+            this->playback_phase = 0;
         this->is_playing = true;
         this->state = SIGNALFLOW_NODE_STATE_ACTIVE;
     }
     else if (name == SIGNALFLOW_TRIGGER_RECORD)
     {
-        if (this->state == SIGNALFLOW_NODE_STATE_STOPPED)
-        {
-            this->phase = 0.0;
-        }
+        this->record_phase = 0.0;
         this->is_recording = true;
         this->state = SIGNALFLOW_NODE_STATE_ACTIVE;
     }
     else if (name == SIGNALFLOW_TRIGGER_SET_POSITION)
     {
-        this->phase = buffer->get_sample_rate() * value;
+        this->playback_phase = buffer->get_sample_rate() * value;
     }
     else
     {
@@ -70,47 +81,68 @@ void BufferLooper::process(Buffer &out, int num_frames)
      *--------------------------------------------------------------------------------*/
     if (!this->buffer || !this->buffer->get_num_frames())
         return;
+    if (this->buffer->get_num_channels() != this->num_input_channels)
+    {
+        signalflow_audio_thread_error("BufferLooper: Buffer channel count does not match the audio input (" + std::to_string(this->buffer->get_num_channels()) + " != " + std::to_string(this->num_input_channels) + ")");
+    }
 
     for (int frame = 0; frame < num_frames; frame++)
     {
         for (int channel = 0; channel < this->num_input_channels; channel++)
         {
-            this->out[channel][frame] = 0.0;
-            if ((unsigned int) this->phase < buffer->get_num_frames())
+            this->out[channel][frame] = this->input->out[channel][frame] * this->playthrough_level->out[0][frame];
+
+            if ((unsigned int) this->playback_phase < buffer->get_num_frames())
+            {
+                if (this->is_playing)
+                {
+                    this->out[channel][frame] += this->buffer->data[channel][(int) this->playback_phase] * this->looper_level->out[channel][frame];
+                }
+            }
+            if ((unsigned int) this->record_phase < buffer->get_num_frames())
             {
                 if (this->is_recording)
                 {
-                    this->buffer->data[channel][(int) this->phase] = (this->feedback->out[channel][frame] * this->buffer->data[channel][(int) this->phase])
+                    this->buffer->data[channel][(int) this->record_phase] = (this->feedback->out[channel][frame] * this->buffer->data[channel][(int) this->record_phase])
                         + this->input->out[channel][frame];
-                }
-                if (this->is_playing)
-                {
-                    this->out[channel][frame] = this->buffer->data[channel][(int) this->phase];
                 }
             }
         }
 
-        this->phase += 1;
-        if (this->phase >= this->buffer->get_num_frames())
+        this->playback_phase += 1;
+        this->record_phase += 1;
+        int last_frame = this->end_time ? (this->end_time->out[0][frame] * this->graph->get_sample_rate()) : this->buffer->get_num_frames();
+        if (this->playback_phase >= last_frame)
         {
-            if (loop_playback || loop_record)
+            if (loop_playback->out[0][frame])
             {
-                while (this->phase >= this->buffer->get_num_frames())
-                    this->phase -= this->buffer->get_num_frames();
-                if (!loop_playback)
-                {
-                    this->is_playing = false;
-                }
-                if (!loop_record)
-                {
-                    this->is_recording = false;
-                }
+                if (this->start_time)
+                    this->playback_phase = this->start_time->out[0][frame] * this->graph->get_sample_rate();
+                else
+                    this->playback_phase = 0;
             }
             else
             {
-                this->set_state(SIGNALFLOW_NODE_STATE_STOPPED);
                 this->is_playing = false;
+                if (!this->is_recording)
+                {
+                    this->set_state(SIGNALFLOW_NODE_STATE_STOPPED);
+                }
+            }
+        }
+        if (this->record_phase >= this->buffer->get_num_frames())
+        {
+            if (loop_record->out[0][frame])
+            {
+                this->record_phase = 0;
+            }
+            else
+            {
                 this->is_recording = false;
+                if (!this->is_playing)
+                {
+                    this->set_state(SIGNALFLOW_NODE_STATE_STOPPED);
+                }
             }
         }
     }
