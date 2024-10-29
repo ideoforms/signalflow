4b2f07d2823f67d3c0687b8e2eb02eb689818168
Daniel Jones <daniel@jones.org.uk>
2024-10-29 16:39:09 +0000

    Add cibuildwheel script for Windows

--- auxiliary/cibuildwheel/make-raspberry-pi-aarch64.sh
+++ auxiliary/cibuildwheel/make-raspberry-pi-aarch64.sh
@@ -6,6 +6,7 @@
 #
 # May be ultimately better to switch to this arm runner action:
 # https://github.com/pguyot/arm-runner-action
+# Or at least cibuildwheel
 #--------------------------------------------------------------------------------
 
 # Fail if any subcommands fail
@@ -21,13 +22,13 @@ do
     python3 -m build --wheel
 done
 
-echo "Renaming to _armv7l..."
 SIGNALFLOW_VERSION=$(cat setup.py | grep version= | tr -dc '0-9.')
 echo "SignalFlow version = $SIGNALFLOW_VERSION"
 cd dist
 mkdir $SIGNALFLOW_VERSION
 for FILE in signalflow-${SIGNALFLOW_VERSION}*_aarch64.whl
 do
+    echo "Renaming to _armv7l..."
     OUTPUT_FILE=$SIGNALFLOW_VERSION/$(echo $FILE | sed 's/aarch64/armv7l/')
     mv $FILE $OUTPUT_FILE
     echo "Created product: dist/$OUTPUT_FILE"
