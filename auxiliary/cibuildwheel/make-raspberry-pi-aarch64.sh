f67d9239bdb55533026b649730e5f4971f806158
Daniel Jones <daniel@jones.org.uk>
2024-11-02 16:35:57 +0000

    Raspberry Pi build: Use auditwheel to create manylinux .whl

--- auxiliary/cibuildwheel/make-raspberry-pi-aarch64.sh
+++ auxiliary/cibuildwheel/make-raspberry-pi-aarch64.sh
@@ -11,8 +11,13 @@
 # Fail if any subcommands fail
 set -e 
 
+curl https://raw.githubusercontent.com/mackron/miniaudio/master/miniaudio.h -o source/include/signalflow/node/io/output/miniaudio-library.h
+
 # Install dependencies to build a fully-fledged Python install
 sudo apt-get install build-essential libffi-dev libreadline-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+
+# Install dependencies to build a manylinux wheel
+sudo apt-get patchelf
 
 VERSIONS="3.8.20 3.9.20 3.10.15 3.11.10 3.12.7 3.13.0"
 
@@ -23,6 +28,8 @@ do
     echo "Building version: $VERSION"
     pyenv local $VERSION
     python3 --version
-    pip3 install build
     python3 -m build --wheel
 done
+
+pip3 install build auditwheel
+auditwheel repair --plat manylinux_2_34_aarch64 dist/*.whl
