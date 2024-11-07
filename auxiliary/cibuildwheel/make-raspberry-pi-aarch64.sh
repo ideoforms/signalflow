c87f298e851e30594b0f8a528cae3f61b5fbe76e
Daniel Jones <daniel@jones.org.uk>
2024-11-07 12:51:14 +0000

    Add patchelf etc

--- auxiliary/cibuildwheel/make-raspberry-pi-aarch64.sh
+++ auxiliary/cibuildwheel/make-raspberry-pi-aarch64.sh
@@ -2,6 +2,7 @@
 
 #--------------------------------------------------------------------------------
 # Build script for Raspberry Pi, which creates Linux/arm builds.
+# Tested on Raspberry Pi 4 running Raspberry Pi OS circa October 2024
 # Requires pyenv, which requires libffi-dev to build Python with ctypes support.
 #
 # May be ultimately better to switch to this arm runner action:
@@ -14,6 +15,10 @@ set -e
 # Install dependencies to build a fully-fledged Python install
 sudo apt-get install build-essential libffi-dev libreadline-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 
+# Dependencies for auditwheel, required to make manylinux build
+sudo apt install patchelf
+pip install auditwheel
+
 VERSIONS="3.8.20 3.9.20 3.10.15 3.11.10 3.12.7 3.13.0"
 
 pyenv install --skip-existing $VERSIONS
@@ -25,3 +30,7 @@ do
     pip3 install build
     python3 -m build --wheel
 done
+
+cd dist
+mkdir -p fixed
+auditwheel repair *.whl -w fixed --plat manylinux_2_34_aarch64
